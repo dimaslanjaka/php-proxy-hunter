b51e85d163fae4c31780a0e20b3b71e17dd9e359
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-13 00:08:44 +0700

    initialize proxy fetcher
    Former-commit-id: 573120a21cd74ec488775897f8f3122a1bc8b244

---
+++ proxyFetcher.php
@@ -0,0 +1,5 @@
+<?php
+
+// https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt
+// https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt
+// https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt
