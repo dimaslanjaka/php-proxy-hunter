10cedc5fa8177e9b2c6765bac0c1024de69505d9
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-12 23:58:19 +0700

    wrap parser into function add hidemy parser
    Former-commit-id: bf7ac706f13e95a2d3e7b721f5a941e59884abde

--- userscripts/spysone.js
+++ userscripts/spysone.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         spys.one proxy parser
-// @namespace    iquaridys:hideme-parser-proxy
-// @version      0.1
+// @namespace    dimaslanjaka:hideme-parser-proxy
+// @version      1.0
 // @description  parse proxy from site page
-// @author       iquaridys
+// @author       dimaslanjaka
 // @match        http://spys.one/*/
 // @downloadURL https://raw.githack.com/dimaslanjaka/php-proxy-hunter/master/userscripts/spysone.js
 // @updateURL   https://raw.githack.com/dimaslanjaka/php-proxy-hunter/master/userscripts/spysone.js
@@ -11,13 +11,16 @@
 
 (function () {
 	"use strict";
-	var resultText = "";
-	var a = document.getElementsByClassName("spy14");
-	for (var i = 0; i < a.length; i++) {
-		if (a[i].innerText.includes(":")) {
-			resultText += a[i].innerText + "<br>";
+	const parse = () => {
+		var resultText = "";
+		var a = document.getElementsByClassName("spy14");
+		for (var i = 0; i < a.length; i++) {
+			if (a[i].innerText.includes(":")) {
+				resultText += a[i].innerText + "<br>";
+			}
 		}
-	}
+		return resultText;
+	};
 
 	const btn = document.createElement("button");
 	btn.setAttribute("style", "position: fixed;bottom: 2em;right: 2em;");
@@ -25,7 +28,7 @@
 	btn.classList.add("btn", "button", "btn-primary");
 	btn.onclick = () => {
 		window.open(
-			URL.createObjectURL(new Blob([resultText], { type: "text/html" })),
+			URL.createObjectURL(new Blob([parse()], { type: "text/html" })),
 			"width=800,height=600"
 		);
 	};
