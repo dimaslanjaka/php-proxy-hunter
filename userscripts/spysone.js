ca5e018958de1c65b7a1a5f23f51f3aa60708bcb
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-12 23:46:38 +0700

    add spys.one extractor
    Former-commit-id: da26fa8c27c48edccc0a9acdbb909b7f1928f02d

---
+++ userscripts/spysone.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         spys.one proxy parser
+// @namespace    iquaridys:hideme-parser-proxy
+// @version      0.1
+// @description  parse proxy from site page
+// @author       iquaridys
+// @match        http://spys.one/*/
+// ==/UserScript==
+
+(function () {
+	"use strict";
+	var resultText = "";
+	var a = document.getElementsByClassName("spy14");
+	for (var i = 0; i < a.length; i++) {
+		if (a[i].innerText.includes(":")) {
+			resultText += a[i].innerText + "<br>";
+		}
+	}
+
+	const btn = document.createElement("button");
+	btn.setAttribute("style", "position: fixed;bottom: 2em;right: 2em;");
+	btn.innerText = "PARSE PROXIES";
+	btn.classList.add("btn", "button", "btn-primary");
+	btn.onclick = () => {
+		window.open(
+			URL.createObjectURL(new Blob([resultText], { type: "text/html" })),
+			"width=800,height=600"
+		);
+	};
+	document.body.appendChild(btn);
+})();
