e6c7d3bae0dc1292b206ed40f4406f7da1411d78
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-04-15 00:42:33 +0700

    Update shortcut.js
    Former-commit-id: 9c56140de203efb15040b676104a9711201e11a9

--- userscripts/shortcut.js
+++ userscripts/shortcut.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jquery HVT
 // @namespace    https://tranghv.blogspot.com/
-// @version      0.1
+// @version      0.2
 // @description  Add jquery turbolink
 // @author       Tráng Hà Viết
 // @match        http://*/*
