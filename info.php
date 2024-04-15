f9f381f7ae751e20957094f326718997bc337ff6
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-15 14:30:15 +0700

    print current user information
    Former-commit-id: 3edef5a27c9f7317ca96f26e53c3c5a8c4c06666

--- info.php
+++ info.php
@@ -1,3 +1,11 @@
 <?php
 
-echo gethostname() == "DESKTOP-JVTSJ6I";
+require_once __DIR__ . '/func.php';
+
+// Allow from any origin
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+header('Content-Type: application/json; charset=utf-8');
+
+echo json_encode(getConfig(getUserId()));
