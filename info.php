e09fcdd419e290e5f0b8e79a16be52a7ce60ead9
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-15 14:58:19 +0700

    set cookie current user info
    Former-commit-id: 70f2683dec8a7c8ec9d1bb4ec974ab93fba2a6eb

--- info.php
+++ info.php
@@ -8,4 +8,24 @@ header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 header('Content-Type: application/json; charset=utf-8');
 
-echo json_encode(getConfig(getUserId()));
+set_cookie("user_id", getUserId());
+
+$config = json_encode(getConfig(getUserId()));
+
+set_cookie("user_config", base64_encode($config));
+
+echo $config;
+
+function set_cookie($name, $value, $expiration_days = 1, $path = "/", $domain = null)
+{
+  // Calculate the expiration time (in seconds)
+  $expiration_time = time() + ($expiration_days * 24 * 60 * 60);
+
+  // Set the domain to current domain if not provided
+  if ($domain === null) {
+    $domain = $_SERVER['HTTP_HOST'];
+  }
+
+  // Set the cookie
+  setcookie($name, $value, $expiration_time, $path, $domain);
+}
