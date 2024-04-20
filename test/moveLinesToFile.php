1d5652f440f563ad8f4a8ce9cd652f2391524eba
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-20 22:44:41 +0700

    add move (n) first lines from file to another
    Former-commit-id: 101d4e10663bc787f0431a6a21bfbf6e474a717d

---
+++ test/moveLinesToFile.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/../func.php';
+
+$sourceFile = __DIR__ . '/../tmp/source.txt';
+$destinationFile = __DIR__ . '/../tmp/destination.txt';
+$linesToMove = 50;
+
+$lines = [];
+
+// Generate 100 random strings
+for ($i = 0; $i < 100; $i++) {
+  $lines[] = generateRandomString();
+}
+
+// if (!file_exists(dirname($sourceFile))) mkdir(dirname($sourceFile));
+// file_put_contents($sourceFile, implode(PHP_EOL, $lines));
+
+if (moveLinesToFile($sourceFile, $destinationFile, $linesToMove)) {
+  echo "First $linesToMove lines moved successfully from '$sourceFile' to '$destinationFile'.";
+} else {
+  echo "Failed to move lines from '$sourceFile' to '$destinationFile'.";
+}
