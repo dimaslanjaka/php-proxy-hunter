ceafa39cd0f55067404cc40b460a5c7967f599af
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-20 17:49:05 +0700

    update lock status
    Former-commit-id: 66877390755c6a67238e701ddbc04f3260afcccd

--- filterPorts.php
+++ filterPorts.php
@@ -9,16 +9,21 @@ use PhpProxyHunter\ProxyDB;
 if (function_exists('header')) header('Content-Type: text/plain; charset=UTF-8');
 
 $lockFilePath = __DIR__ . "/proxyChecker.lock";
+$statusFile = __DIR__ . "/status.txt";
 
 if (file_exists($lockFilePath)) {
-  exit('another process still running');
+  echo "another process still running\n";
+  exit();
 } else {
-  file_put_contents($lockFilePath, '');
+  file_put_contents($lockFilePath, date(DATE_RFC3339));
+  file_put_contents($statusFile, 'running');
 }
+
 function exitProcess()
 {
   global $lockFilePath, $statusFile;
   if (file_exists($lockFilePath)) unlink($lockFilePath);
+  file_put_contents($statusFile, 'idle');
 }
 register_shutdown_function('exitProcess');
 
@@ -27,6 +32,7 @@ $db = new ProxyDB();
 $file = __DIR__ . '/proxies.txt';
 removeEmptyLinesFromFile($file);
 $proxies = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+// shuffle proxies
 shuffle($proxies);
 // $openPortsOnly = array_filter($proxies, 'isPortOpen');
 // file_put_contents(__DIR__ . '/tmp/test.txt', join(PHP_EOL, $openPortsOnly));
