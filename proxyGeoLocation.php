21e49d4beee5598a0e12d07a99e6357566696f09
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-21 21:40:45 +0700

    remove unused files
    Former-commit-id: e1ae378a0102674126c2614324713e4aafb5102f

--- proxyGeoLocation.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$LocationArray = json_decode(file_get_contents('http://ip-get-geolocation.com/api/json/35.188.125.133'), true);
-
-echo $LocationArray['country'];
-echo $LocationArray['city'];
-echo $LocationArray['region'];
-echo $LocationArray['timezone'];
