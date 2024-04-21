74538869188bc45e0495564e0f5a3b26d1ec8eac
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-21 22:04:08 +0700

    simplify sanitize filename
    Former-commit-id: bb1f1d8ce52058cc842638310759bf086316ccb7

--- func.php
+++ func.php
@@ -698,21 +698,7 @@ function removeStringFromFile($file_path, $string_to_remove)
   return $result !== false;
 }
 
-function sanitizeFilename($name)
-{
-  $name = str_replace('/', '-', $name);
-  // remove illegal file system characters https://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
-  $name = str_replace(array_merge(
-    array_map('chr', range(0, 31)),
-    array('<', '>', ':', '"', '/', '\\', '|', '?', '*')
-  ), '', $name);
-  // maximise filename length to 255 bytes http://serverfault.com/a/9548/44086
-  $ext = pathinfo($name, PATHINFO_EXTENSION);
-  $name = mb_strcut(pathinfo($name, PATHINFO_FILENAME), 0, 255 - ($ext ? strlen($ext) + 1 : 0), mb_detect_encoding($name)) . ($ext ? '.' . $ext : '');
-  return $name;
-}
-
-function sanitizeFilename2($filename)
+function sanitizeFilename($filename)
 {
   // Remove any character that is not alphanumeric, underscore, dash, or period
   $filename = preg_replace("/[^\w\-\. ]/", '-', $filename);
