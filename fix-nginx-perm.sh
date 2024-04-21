8cda181baf2a3606af62a4828d5f4054cb9f82ff
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-21 22:28:47 +0700

    create empty index and change permission tmp
    Former-commit-id: bf0588767dfcc10d6d8c28e8af04d658ca0734a0

--- fix-nginx-perm.sh
+++ fix-nginx-perm.sh
@@ -4,10 +4,13 @@ sudo chown -R www-data:www-data *
 mkdir -p tmp/cookies
 touch tmp/cookies/index.html
 touch tmp/index.html
+mkdir -p config
+touch config/index.html
 sudo chown -R www-data:www-data tmp/*
 sudo chown -R www-data:www-data tmp/cookies/*
 sudo chmod 777 *.txt
 sudo chmod 777 config
+sudo chmod 777 tmp
 sudo chmod 777 .cache
 sudo chmod 644 *.php
 # sudo chown -R www-data:www-data /var/www/html
