26ea70584f20574c23303fd075839b2da6128351
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-22 01:32:07 +0700

    fix writing large file
    Former-commit-id: 55fac5ea0047c401f2e8d9e1c3658c98bb26b7c8

--- CIDR-ports.php
+++ CIDR-ports.php
@@ -1,11 +1,28 @@
 <?php
 
-// scan ports from generated ip ranges
+// generate ports from generated ip ranges
 
 require_once __DIR__ . '/func.php';
 
+// disallow web server access
+if (php_sapi_name() !== 'cli') {
+  // Redirect the user away or show an error message
+  header('HTTP/1.1 403 Forbidden');
+  die('Direct access not allowed');
+}
+
+// set memory
+ini_set('memory_limit', '2024M');
+
 $filePath = getRandomFileFromFolder(__DIR__ . '/tmp/ips', 'txt');
+$outputPath = __DIR__ . '/tmp/ips-ports/' . basename($filePath);
+if (!file_exists(dirname($outputPath))) mkdir(dirname($outputPath));
 $ipList = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+if (empty($ipList)) {
+  unlink($filePath);
+  exit('ips empty');
+}
 
 $commonPorts = [
   80, 81, 83, 88, 3128, 3129, 3654, 4444, 5800, 6588, 6666,
@@ -36,120 +53,30 @@ foreach ($ipList as $ip) {
   }
 
   $ip = trim($ip);
-  echo "scan ports $ip" . PHP_EOL;
+
+  $ip_ports = array_map(function ($port) use ($ip) {
+    return "$ip:$port";
+  }, $commonPorts);
+
+  // write generated IP:PORT
+  append_content_with_lock($outputPath, PHP_EOL . implode(PHP_EOL, $ip_ports) . PHP_EOL);
+  // remove ip
+  removeStringFromFile($filePath, trim($ip));
+
+  // echo "scan ports $ip" . PHP_EOL;
   // scan common proxy ports
-  $proxies = scanArrayPorts($ip, $commonPorts);
+  // $proxies = scanArrayPorts($ip, $commonPorts);
   // scan all ports
   // $proxies = array_unique(array_merge($proxies, scanRangePorts($ip, 80, 65535)));
-  if (!empty($proxies)) {
-    // remove checked ip from source
-    removeStringFromFile($filePath, trim($ip));
-    // write open IP:PORT into test files
-    append_content_with_lock(__DIR__ . '/proxies.txt', PHP_EOL . implode(PHP_EOL, $proxies) . PHP_EOL);
-    append_content_with_lock(__DIR__ . '/proxies-backup.txt', PHP_EOL . implode(PHP_EOL, $proxies) . PHP_EOL);
-  }
+  // if (!empty($proxies)) {
+  //   // remove checked ip from source
+  //   removeStringFromFile($filePath, trim($ip));
+  //   // write open IP:PORT into test files
+  //   append_content_with_lock(__DIR__ . '/proxies.txt', PHP_EOL . implode(PHP_EOL, $proxies) . PHP_EOL);
+  //   append_content_with_lock(__DIR__ . '/proxies-backup.txt', PHP_EOL . implode(PHP_EOL, $proxies) . PHP_EOL);
+  // }
 }
 
-rewriteIpPortFile(__DIR__ . '/proxies.txt');
+rewriteIpPortFile($outputPath);
 
-/**
- * Scans a range of ports on a given IP address and returns an array of proxies.
- *
- * @param string $ip The IP address to scan ports on.
- * @param int $startPort The starting port of the range (default is 1).
- * @param int $endPort The ending port of the range (default is 65535).
- * @return array An array containing the proxies found during scanning.
- */
-function scanRangePorts(string $ip, int $startPort = 1, int $endPort = 65535)
-{
-  $proxies = [];
-  for ($port = $startPort; $port <= $endPort; $port++) {
-    if (scanPort($ip, $port)) {
-      $proxies[] = "$ip:$port";
-    }
-  }
-  return $proxies;
-}
-
-/**
- * Scans an array of specific ports on a given IP address and returns an array of proxies.
- *
- * @param string $ip The IP address to scan ports on.
- * @param array $ports An array containing the ports to scan.
- * @return array An array containing the proxies found during scanning.
- */
-function scanArrayPorts(string $ip, array $ports)
-{
-  $proxies = [];
-  foreach ($ports as $port) {
-    if (scanPort($ip, $port)) {
-      $proxies[] = "$ip:$port";
-    }
-  }
-  return $proxies;
-}
-
-/**
- * Scans a specific port on a given IP address.
- *
- * @param string $ip The IP address to scan the port on.
- * @param int $port The port to scan.
- * @return bool Returns true if the port is open, false otherwise.
- */
-function scanPort(string $ip, int $port)
-{
-  $ip = trim($ip);
-  echo "Scanning port $ip:$port\n";
-  $connection = @fsockopen($ip, $port, $errno, $errstr, 10);
-  if (is_resource($connection)) {
-    echo "Port $port is open.\n";
-    fclose($connection);
-    return true;
-  }
-  return false;
-}
-
-// $ip = "127.0.0.1"; // Change this to the IP you want to scan
-// $startPort = 1; // Starting port number
-// $endPort = 65535; // Ending port number
-
-// scanPorts($ip, $startPort, $endPort);
-
-/**
- * Get a random file from a folder.
- *
- * @param string $folder The path to the folder containing files.
- * @param string|null $file_extension The optional file extension without dot (.) to filter files by.
- * @return string|null The name of the randomly selected file, or null if no file found with the specified extension.
- */
-function getRandomFileFromFolder($folder, $file_extension = null)
-{
-  // Get list of files in the folder
-  $files = scandir($folder);
-
-  // Remove special directories "." and ".." from the list
-  $files = array_diff($files, array('.', '..'));
-
-  // Filter files by extension if provided
-  if ($file_extension !== null) {
-    $files = array_filter($files, function ($file) use ($file_extension) {
-      return pathinfo($file, PATHINFO_EXTENSION) == $file_extension;
-    });
-  }
-
-  // Get number of files
-  $num_files = count($files);
-
-  // Check if there are files with the specified extension
-  if ($num_files === 0) {
-    return null; // No files found with the specified extension
-  }
-
-  // Generate a random index
-  $random_index = mt_rand(0, $num_files - 1);
-
-  // Get the randomly selected file
-  $random_file = $files[$random_index];
-
-  return $folder . '/' . $random_file;
-}
+// rewriteIpPortFile(__DIR__ . '/proxies.txt');
