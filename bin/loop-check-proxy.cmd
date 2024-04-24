28032aab92821e012cf48194cd34de28802ea090
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-24 19:46:13 +0700

    trigger working proxy script
    Former-commit-id: 596fd7a8a15e141eee59d33505bc45e9c85ac9d6

--- bin/loop-check-proxy.cmd
+++ bin/loop-check-proxy.cmd
@@ -1,7 +1,8 @@
 @echo off
 
-for /l %%x in (1, 1, 100) do (
+for /l %%x in (1, 1, 3) do (
    echo "run proxy checking #%%x"
    echo:
-   php proxyCheckerBackground.php
+   check-proxy
+   echo:
 )
