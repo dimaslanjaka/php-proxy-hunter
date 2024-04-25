d1f1725e6f9b1e6c90a9dbfe73af4f12cc6d1d16
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-25 23:14:55 +0700

    `--max` argument not working

--- bin/loop-check-proxy.cmd
+++ bin/loop-check-proxy.cmd
@@ -1,16 +1,13 @@
 @echo off
 
-@REM REM loop-check-proxy --max 5
+@REM REM loop-check-proxy 5
 
 REM Set default value for max
 set max=1
 
-REM Check if --max argument is provided and set max accordingly
+REM Check if numeric argument is provided and set max accordingly
 if not "%~1"=="" (
-    set max=%~1
-    echo Max set to %max%
-) else (
-    echo Max not provided, using default value of %max%
+    set max=%1
 )
 
 REM Run the scripts for the specified number of times
