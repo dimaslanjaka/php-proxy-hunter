5a4c0bb12f00fb32dd62322b66aa0aff30e5f044
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-25 23:09:58 +0700

    add `--max` argument for max loop

--- bin/loop-check-proxy.cmd
+++ bin/loop-check-proxy.cmd
@@ -1,8 +1,24 @@
 @echo off
 
-for /l %%x in (1, 1, 3) do (
-   echo "run proxy checking #%%x"
-   echo.
-   check-proxy > NUL
-   echo.
+@REM REM loop-check-proxy --max 5
+
+REM Set default value for max
+set max=1
+
+REM Check if --max argument is provided and set max accordingly
+if not "%~1"=="" (
+    set max=%~1
+    echo Max set to %max%
+) else (
+    echo Max not provided, using default value of %max%
 )
+
+REM Run the scripts for the specified number of times
+for /l %%i in (1, 1, %max%) do (
+    echo Running iteration %%i of %max%
+    php proxyCheckerBackground.php
+    echo.
+    php proxyWorking.php
+)
+
+exit /b 0
