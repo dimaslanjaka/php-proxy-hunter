dc1fede3f6d76e2ffe16c1eb0e0033cf8e4e2003
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-01 10:19:10 +0700

    update from other branch

--- bin/check-proxy.cmd
+++ bin/check-proxy.cmd
@@ -1,5 +1,14 @@
 @echo off
 
-php proxyCheckerBackground.php
+set CUSTOM_PHP_PATH=..\assets\php\php.exe
+
+rem Check if custom PHP exists, if yes, set it as PHP_PATH
+if exist "%CUSTOM_PHP_PATH%" (
+    set PHP_PATH="%CUSTOM_PHP_PATH%"
+) else (
+    set PHP_PATH=php
+)
+
+%PHP_PATH% proxyCheckerBackground.php
 echo.
-php proxyWorking.php
+%PHP_PATH% proxyWorking.php
