bd9adeac05e4707480840385bcb835c9977307e3
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-01 13:21:08 +0700

    fix certificate permission

--- fix-nginx-perm.sh
+++ fix-nginx-perm.sh
@@ -10,12 +10,15 @@ touch tmp/cookies/index.html
 touch tmp/index.html
 mkdir -p config
 touch config/index.html
+sudo chown -R www-data:www-data data/*
 sudo chown -R www-data:www-data tmp/*
 sudo chown -R www-data:www-data tmp/cookies/*
 sudo chmod 777 *.txt
 sudo chmod 777 config
 sudo chmod 777 tmp
 sudo chmod 777 .cache
+sudo chmod 777 data
+sudo chmod 777 data/*.pem
 sudo chmod 644 *.php
 # sudo chown -R www-data:www-data /var/www/html
 
