1e955575d0ecf3bcf072e63f64a7e82ad7fe91fd
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-02 20:17:19 +0700

    update php doc and type

--- src/ProxyDB.php
+++ src/ProxyDB.php
@@ -4,38 +4,80 @@ namespace PhpProxyHunter;
 
 if (!defined('PHP_PROXY_HUNTER')) exit('access denied');
 
+/**
+ * Class ProxyDB
+ *
+ * @package PhpProxyHunter
+ */
 class ProxyDB
 {
+  /** @var SQLiteHelper $db */
   private $db;
-  public function __construct($dbLocation = __DIR__ . '/database.sqlite')
+
+  /**
+   * ProxyDB constructor.
+   *
+   * @param string $dbLocation
+   */
+  public function __construct(string $dbLocation = __DIR__ . '/database.sqlite')
   {
     $this->db = new SQLiteHelper($dbLocation);
   }
 
-  public function select(string $proxy)
+  /**
+   * Selects a proxy from the database.
+   *
+   * @param string $proxy
+   * @return array
+   */
+  public function select(string $proxy): array
   {
     return $this->db->select('proxies', '*', 'proxy = ?', [trim($proxy)]);
   }
 
-  public function getAllProxies()
+  /**
+   * Get all proxies from the database.
+   *
+   * @return array
+   */
+  public function getAllProxies(): array
   {
     return $this->db->select('proxies', '*');
   }
 
-  public function remove(string $proxy)
+  /**
+   * Removes a proxy from the database.
+   *
+   * @param string $proxy
+   */
+  public function remove(string $proxy): void
   {
     $this->db->delete('proxies', 'proxy = ?', [trim($proxy)]);
   }
 
-  public function add(string $proxy)
+  /**
+   * Adds a proxy to the database.
+   *
+   * @param string $proxy
+   */
+  public function add(string $proxy): void
   {
     $this->db->insert('proxies', ['proxy' => trim($proxy), 'status' => 'untested']);
   }
 
   /**
-   * insert proxy or update when not exist
+   * Inserts or updates a proxy in the database.
+   *
+   * @param string $proxy
+   * @param string|null $type
+   * @param string|null $region
+   * @param string|null $city
+   * @param string|null $country
+   * @param string|null $status
+   * @param string|null $latency
+   * @param string|null $timezone
    */
-  public function update(string $proxy, string $type = null, string $region = null, string $city = null, string $country = null, string $status = null, string $latency = null, string $timezone = null)
+  public function update(string $proxy, ?string $type = null, ?string $region = null, ?string $city = null, ?string $country = null, ?string $status = null, ?string $latency = null, ?string $timezone = null): void
   {
     if (empty($this->select($proxy))) {
       $this->add($proxy);
@@ -54,7 +96,13 @@ class ProxyDB
     if (!empty($data)) $this->updateData($proxy, $data);
   }
 
-  public function updateData(string $proxy, array $data = [])
+  /**
+   * Updates data for a specific proxy.
+   *
+   * @param string $proxy
+   * @param array $data
+   */
+  public function updateData(string $proxy, array $data = []): void
   {
     if (empty($this->select($proxy))) {
       $this->add($proxy);
@@ -67,24 +115,46 @@ class ProxyDB
     if (!empty($data)) $this->db->update('proxies', $data, 'proxy = ?', [trim($proxy)]);
   }
 
-  public function updateStatus(string $proxy, string $status)
+  /**
+   * Updates the status of a proxy.
+   *
+   * @param string $proxy
+   * @param string $status
+   */
+  public function updateStatus(string $proxy, string $status): void
   {
     $this->update(trim($proxy), null, null, null, null, $status, null);
   }
 
-  public function updateLatency(string $proxy, string $latency)
+  /**
+   * Updates the latency of a proxy.
+   *
+   * @param string $proxy
+   * @param string $latency
+   */
+  public function updateLatency(string $proxy, string $latency): void
   {
     $this->update(trim($proxy), null, null, null, null, null, $latency);
   }
 
-  public function getWorkingProxies()
+  /**
+   * Gets all working proxies from the database.
+   *
+   * @return array
+   */
+  public function getWorkingProxies(): array
   {
     $result = $this->db->select('proxies', '*', 'status = ?', ['active']);
     if (!$result) return [];
     return $result;
   }
 
-  public function getPrivateProxies()
+  /**
+   * Gets all private proxies from the database.
+   *
+   * @return array
+   */
+  public function getPrivateProxies(): array
   {
     $result = $this->db->select('proxies', '*', 'status = ?', ['private']);
     if (!$result) return [];
@@ -92,9 +162,11 @@ class ProxyDB
   }
 
   /**
-   * get dead proxies including closed port
+   * Gets all dead proxies from the database, including those with closed ports.
+   *
+   * @return array
    */
-  public function getDeadProxies()
+  public function getDeadProxies(): array
   {
     $result = $this->db->select('proxies', '*', 'status = ?', ['dead']);
     $result2 = $this->db->select('proxies', '*', 'status = ?', ['port-closed']);
