b0745d1acd7312ea7477bd7585a70caf6b50af21
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-02 19:44:24 +0700

    show fake date when not exist

--- data/index.php
+++ data/index.php
@@ -14,10 +14,8 @@ if (isset($_REQUEST['uid'])) {
     if (isset($_REQUEST['create'])) {
       $currentDate = new DateTime();
       $currentDate->add(new DateInterval('P1W')); // Adding 1 week
-
       // Format the date according to RFC3339
       $oneWeekLater = $currentDate->format(DateTime::RFC3339); // date(DATE_RFC3339)
-
       file_put_contents($user_file, json_encode(['valid_until' => $oneWeekLater]));
     }
   }
@@ -25,5 +23,16 @@ if (isset($_REQUEST['uid'])) {
     $data_str = file_get_contents($user_file);
     $data = json_decode($data_str, true);
     exit($data_str);
+  } else {
+    $dateString = '2011-11-11';
+    $date = DateTime::createFromFormat('Y-m-d', $dateString);
+
+    if ($date !== false) {
+      // Format the date according to RFC3339
+      $formattedDate = $date->format(DateTime::RFC3339);
+      exit(json_encode(['valid_until' => $formattedDate]));
+    } else {
+      exit(json_encode(['valid_until' => '2012-12-12T19:43:02+07:00']));
+    }
   }
 }
