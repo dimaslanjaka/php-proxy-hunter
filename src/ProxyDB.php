ec2a2399eee8864d895c32bf27111913cdd8bb72
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-04 11:32:24 +0700

    add proxy class

--- src/ProxyDB.php
+++ src/ProxyDB.php
@@ -175,3 +175,142 @@ class ProxyDB
     return array_merge($result, $result2);
   }
 }
+
+/**
+ * proxy table data class
+ */
+class Proxy
+{
+  /** @var int|null */
+  public $id = null;
+
+  /** @var string */
+  public $proxy;
+
+  /** @var string|null */
+  public $latency = null;
+
+  /** @var string|null */
+  public $type = null;
+
+  /** @var string|null */
+  public $region = null;
+
+  /** @var string|null */
+  public $city = null;
+
+  /** @var string|null */
+  public $country = null;
+
+  /** @var string|null */
+  public $last_check = null;
+
+  /** @var string|null */
+  public $anonymity = null;
+
+  /** @var string|null */
+  public $status = null;
+
+  /** @var string|null */
+  public $timezone = null;
+
+  /** @var string|null */
+  public $longitude = null;
+
+  /** @var string|null */
+  public $private = null;
+
+  /** @var string|null */
+  public $latitude = null;
+
+  /** @var string|null */
+  public $lang = null;
+
+  /** @var string|null */
+  public $useragent = null;
+
+  /** @var string|null */
+  public $webgl_vendor = null;
+
+  /** @var string|null */
+  public $webgl_renderer = null;
+
+  /** @var string|null */
+  public $browser_vendor = null;
+
+  /** @var string|null */
+  public $username = null;
+
+  /** @var string|null */
+  public $password = null;
+
+  /**
+   * Proxy constructor.
+   * @param string $proxy
+   * @param string|null $latency
+   * @param string|null $type
+   * @param string|null $region
+   * @param string|null $city
+   * @param string|null $country
+   * @param string|null $last_check
+   * @param string|null $anonymity
+   * @param string|null $status
+   * @param string|null $timezone
+   * @param string|null $longitude
+   * @param string|null $private
+   * @param string|null $latitude
+   * @param string|null $lang
+   * @param string|null $useragent
+   * @param string|null $webgl_vendor
+   * @param string|null $webgl_renderer
+   * @param string|null $browser_vendor
+   * @param string|null $username
+   * @param string|null $password
+   * @param int|null $id
+   */
+  public function __construct(
+    string $proxy,
+    ?string $latency = null,
+    ?string $type = null,
+    ?string $region = null,
+    ?string $city = null,
+    ?string $country = null,
+    ?string $last_check = null,
+    ?string $anonymity = null,
+    ?string $status = null,
+    ?string $timezone = null,
+    ?string $longitude = null,
+    ?string $private = null,
+    ?string $latitude = null,
+    ?string $lang = null,
+    ?string $useragent = null,
+    ?string $webgl_vendor = null,
+    ?string $webgl_renderer = null,
+    ?string $browser_vendor = null,
+    ?string $username = null,
+    ?string $password = null,
+    ?int $id = null
+  ) {
+    $this->id = $id;
+    $this->proxy = $proxy;
+    $this->latency = $latency;
+    $this->type = $type;
+    $this->region = $region;
+    $this->city = $city;
+    $this->country = $country;
+    $this->last_check = $last_check;
+    $this->anonymity = $anonymity;
+    $this->status = $status;
+    $this->timezone = $timezone;
+    $this->longitude = $longitude;
+    $this->private = $private;
+    $this->latitude = $latitude;
+    $this->lang = $lang;
+    $this->useragent = $useragent;
+    $this->webgl_vendor = $webgl_vendor;
+    $this->webgl_renderer = $webgl_renderer;
+    $this->browser_vendor = $browser_vendor;
+    $this->username = $username;
+    $this->password = $password;
+  }
+}
