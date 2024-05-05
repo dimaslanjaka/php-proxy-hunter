8dea1e15785d71dcba011a4678e2fd9553691e8c
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-05 23:27:14 +0700

    add admin info from 'data/login.php'

--- info.php
+++ info.php
@@ -9,12 +9,14 @@ header("Access-Control-Allow-Methods: *");
 header('Content-Type: application/json; charset=utf-8');
 
 set_cookie("user_id", getUserId());
+$config = getConfig(getUserId());
+// admin info from 'data/login.php'
+$config['admin'] = isset($_SESSION['admin']) && $_SESSION['admin'] == true;
+$config_json = json_encode($config);
 
-$config = json_encode(getConfig(getUserId()));
+set_cookie("user_config", base64_encode($config_json));
 
-set_cookie("user_config", base64_encode($config));
-
-echo $config;
+echo $config_json;
 
 function set_cookie($name, $value, $expiration_days = 1, $path = "/", $domain = null)
 {
