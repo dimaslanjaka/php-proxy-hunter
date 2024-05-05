0e97ce75484ef9b18696d2b9e7e4e47d6499703a
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-06 05:52:10 +0700

    add geo ip background fetch

--- geoIp.php
+++ geoIp.php
@@ -2,13 +2,7 @@
 
 require_once __DIR__ . '/func-proxy.php';
 
-if (file_exists(__DIR__ . '/proxyChecker.lock') && gethostname() !== 'DESKTOP-JVTSJ6I') {
-  exit('proxy checker process still running');
-}
-
-if (function_exists('set_time_limit')) {
-  call_user_func('set_time_limit', 120);
-}
+$isCli = strtolower(php_sapi_name()) === 'cli';
 
 if (function_exists('header')) {
   header('Content-Type: application/json; charset=UTF-8');
@@ -18,16 +12,33 @@ if (function_exists('header')) {
   header("Access-Control-Allow-Headers: *");
   header("Access-Control-Allow-Methods: *");
   header('Content-Type: application/json; charset=utf-8');
+}
 
+if (file_exists(__DIR__ . '/proxyChecker.lock') && gethostname() !== 'DESKTOP-JVTSJ6I') {
+  exit('proxy checker process still running');
+}
+
+if (function_exists('header')) {
   // Set cache control headers to instruct the browser to cache the content for [n] hour
   $hour = 1;
   header('Cache-Control: max-age=3600, must-revalidate');
   header('Expires: ' . gmdate('D, d M Y H:i:s', time() + ($hour * 3600)) . ' GMT');
 }
 
+if (function_exists('set_time_limit')) {
+  call_user_func('set_time_limit', 120);
+}
+
+$config = getConfig(getUserId());
+$options = getopt("", ["str:"]); // php geoIp.php --str "xsdsd dfdfd"
+
 $string_data = '112.30.155.83:12792';
-if (strtolower(php_sapi_name()) === 'cli') {
-  $string_data = file_get_contents(__DIR__ . '/proxies.txt');
+if ($isCli) {
+  if (isset($options['str'])) {
+    $string_data = $options['str'];
+  } else {
+    $string_data = file_get_contents(__DIR__ . '/proxies.txt');
+  }
 } else if (isset($_REQUEST['proxy'])) {
   $string_data = $_REQUEST['proxy'];
 }
