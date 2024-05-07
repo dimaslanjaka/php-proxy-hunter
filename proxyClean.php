8a10a9667500a3e3234f822c7470f0c9c652c90e
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-08 06:20:55 +0700

    update proxy cleaner

--- proxyClean.php
+++ proxyClean.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . "/func.php";
+require_once __DIR__ . "/func-proxy.php";
 
 $isCli = (php_sapi_name() === 'cli' || defined('STDIN') || (empty($_SERVER['REMOTE_ADDR']) && !isset($_SERVER['HTTP_USER_AGENT']) && count($_SERVER['argv']) > 0));
 
@@ -15,41 +15,43 @@ $all = __DIR__ . '/proxies-all.txt';
 
 // Define file paths array
 $files = [
-  __DIR__ . '/proxies.txt',
-  __DIR__ . '/working.txt',
-  __DIR__ . '/dead.txt',
-  __DIR__ . '/socks.txt',
-  __DIR__ . '/socks-working.txt',
-  __DIR__ . '/socks-dead.txt',
+    __DIR__ . '/proxies.txt',
+    __DIR__ . '/working.txt',
+    __DIR__ . '/dead.txt'
 ];
 
-setFilePermissions($all);
+setFilePermissions(array_merge($files, [$all]));
 
-// Open all files in read mode
-$all_content = file_get_contents($all);
-
-// Merge contents
-$merged_content = $all_content . PHP_EOL;
-
-// Iterate through the files array and truncate each file
 foreach ($files as $file) {
-  if (file_exists($file)) {
-    $content = file_get_contents($file);
-    $merged_content = $merged_content . PHP_EOL . $content . PHP_EOL;
-    truncateFile($file);
+  echo "processing $file" . PHP_EOL;
+  echo "remove lines not contains IP:PORT" . PHP_EOL;
+
+  try {
+    filterIpPortLines($file);
+  } catch (InvalidArgumentException $e) {
+    echo "Lines not containing IP:PORT format remove failed. " . $e->getMessage() . PHP_EOL;
+  }
+
+  echo "remove empty lines" . PHP_EOL;
+
+  try {
+    removeEmptyLinesFromFile($file);
+  } catch (\Throwable $th) {
+    echo 'Error fix bad contents from proxies.txt: ' . $th->getMessage() . PHP_EOL;
+  }
+
+  echo "fix file NUL" . PHP_EOL;
+
+  try {
+    fixFile($file);
+  } catch (\Throwable $th) {
+    echo 'Error fix bad contents from proxies.txt: ' . $th->getMessage() . PHP_EOL;
+  }
+
+  if (confirmAction("Are you want move $file content into $all:\t")) {
+    $content = read_file($file);
+    append_content_with_lock($all, $content);
   }
 }
 
-// Write merged content to $all
-file_put_contents($all, $merged_content);
 
-// unique proxies
-removeDuplicateLines($all);
-
-echo "Contents merged and moved to $all successfully.";
-
-// Function to truncate the content of a file
-function truncateFile($filePath)
-{
-  file_put_contents($filePath, ''); // Write an empty string to truncate the file
-}
