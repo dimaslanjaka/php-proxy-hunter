ceaf0b1cb47af3dce444be53d26046680424f05d
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-08 17:56:47 +0700

    fix removeStringAndMoveToFile

---
+++ tests/removeStringAndMoveToFile.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../func.php';
+
+$tmp = __DIR__ . '/../tmp/tests';
+$input = $tmp . '/input-removeStringAndMoveToFile.txt';
+$output = $tmp . '/output-removeStringAndMoveToFile.txt';
+createParentFolders($input);
+file_put_contents($output, '');
+$str_to_remove = "this will be deleted";
+file_put_contents($input, implode(PHP_EOL, [
+    $str_to_remove,
+    randomWindowsUa(),
+    randomWindowsUa()
+]));
+$run = removeStringAndMoveToFile($input, $output, $str_to_remove);
+var_dump($run);
