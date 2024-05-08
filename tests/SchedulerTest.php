3e2216d15a18c306cffd2f450e53986d181ad95c
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-08 08:19:53 +0700

    add scheduler test

---
+++ tests/SchedulerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../func-proxy.php';
+
+\PhpProxyHunter\Scheduler::register(function () {
+  echo "Hello world" . PHP_EOL;
+});
+
+exit(0);
+
+print("No response");
