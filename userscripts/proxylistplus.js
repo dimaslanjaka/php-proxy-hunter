002362732a2bb8c894e95b0de324d7bb54faaf8e
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-09 07:07:35 +0700

    add button id

--- userscripts/proxylistplus.js
+++ userscripts/proxylistplus.js
@@ -16,30 +16,30 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
+  "use strict";
   /**
    * @returns {Promise<any[]>}
    */
   const parse = () => {
     return new Promise((resolve) => {
       // Select all table elements on the page
-      const tables = Array.from(document.querySelectorAll('table'));
+      const tables = Array.from(document.querySelectorAll("table"));
       const ipOnly = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/gm;
       const objectWrapper = [];
 
       // Loop through each table element using a for loop
       for (let i = 0; i < tables.length; i++) {
         const table = tables[i];
-        const rows = Array.from(table.querySelectorAll('tr'));
+        const rows = Array.from(table.querySelectorAll("tr"));
 
         for (let j = 0; j < rows.length; j++) {
           const row = rows[j];
-          const td = Array.from(row.querySelectorAll('td'));
-          const texts = td.map((el) => el.innerText).filter((str) => typeof str == 'string' && str.trim().length > 0);
-          if (ipOnly.test(texts.join(' '))) {
+          const td = Array.from(row.querySelectorAll("td"));
+          const texts = td.map((el) => el.innerText).filter((str) => typeof str == "string" && str.trim().length > 0);
+          if (ipOnly.test(texts.join(" "))) {
             // console.log(texts);
             objectWrapper.push({
-              raw: texts[0] + ':' + texts[1],
+              raw: texts[0] + ":" + texts[1],
               ip: texts[0],
               port: texts[1],
               type: texts[2],
@@ -58,14 +58,15 @@
   //   parse();
   // }, 3000);
 
-  const btn = document.createElement('button');
-  btn.setAttribute('style', 'position: fixed; bottom: 50%; right: 2em; transform: translateY(50%);');
-  btn.innerText = 'PARSE PROXIES';
-  btn.classList.add('btn', 'button', 'btn-primary');
+  const btn = document.createElement("button");
+  btn.id = "php-proxy-hunter-grab-proxy";
+  btn.setAttribute("style", "position: fixed; bottom: 50%; right: 2em; transform: translateY(50%);");
+  btn.innerText = "PARSE PROXIES";
+  btn.classList.add("btn", "button", "btn-primary");
   btn.onclick = () => {
     parse().then((result) => {
-      const html = result.map((obj) => JSON.stringify(obj)) + '<br/>';
-      window.open(URL.createObjectURL(new Blob([html], { type: 'text/html' })), 'width=800,height=600');
+      const html = result.map((obj) => JSON.stringify(obj)) + "<br/>";
+      window.open(URL.createObjectURL(new Blob([html], { type: "text/html" })), "width=800,height=600");
     });
   };
   document.body.appendChild(btn);
