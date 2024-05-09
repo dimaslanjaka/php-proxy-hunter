77850ae96299893eebb7730d745b245d734c9232
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-09 07:07:35 +0700

    add button id

--- userscripts/hidemy.js
+++ userscripts/hidemy.js
@@ -13,43 +13,47 @@
 // ==/UserScript==
 
 (function () {
-	"use strict";
+  "use strict";
 
-	$(".table_block>table>tbody>tr").on("click", function () {
-		let tds = $(this).children("td");
-		let ip = tds.eq(0).text();
-		let port = tds.eq(1).text();
-		let host = ip + ":" + port;
-		GM_setClipboard(host);
-		GM_notification({
-			title: "Copied",
-			text: host,
-			timeout: 2000,
-			silent: true,
-		});
-	});
+  // eslint-disable-next-line no-undef
+  $(".table_block>table>tbody>tr").on("click", function () {
+    // eslint-disable-next-line no-undef
+    let tds = $(this).children("td");
+    let ip = tds.eq(0).text();
+    let port = tds.eq(1).text();
+    let host = ip + ":" + port;
+    // eslint-disable-next-line no-undef
+    GM_setClipboard(host);
+    // eslint-disable-next-line no-undef
+    GM_notification({
+      title: "Copied",
+      text: host,
+      timeout: 2000,
+      silent: true
+    });
+  });
 
-	const parseNow = () => {
-		var resultText = "";
-		$(".table_block>table>tbody>tr").each(function (i, e) {
-			var tr = $(e);
-			var tdList = tr.children("td");
-			var host = tdList.get(0).innerText;
-			var port = tdList.get(1).innerText;
-			resultText += host + ":" + port + "<br>";
-		});
-		return resultText;
-	};
+  const parseNow = () => {
+    var resultText = "";
+    // eslint-disable-next-line no-undef
+    $(".table_block>table>tbody>tr").each(function (i, e) {
+      // eslint-disable-next-line no-undef
+      var tr = $(e);
+      var tdList = tr.children("td");
+      var host = tdList.get(0).innerText;
+      var port = tdList.get(1).innerText;
+      resultText += host + ":" + port + "<br>";
+    });
+    return resultText;
+  };
 
-	const btn = document.createElement("button");
-	btn.setAttribute("style", "position: fixed;bottom: 2em;left: 2em;");
-	btn.innerText = "PARSE PROXIES";
-	btn.classList.add("btn", "button", "btn-primary");
-	btn.onclick = () => {
-		window.open(
-			URL.createObjectURL(new Blob([parseNow()], { type: "text/html" })),
-			"width=800,height=600"
-		);
-	};
-	document.body.appendChild(btn);
+  const btn = document.createElement("button");
+  btn.id = "php-proxy-hunter-grab-proxy";
+  btn.setAttribute("style", "position: fixed;bottom: 2em;left: 2em;");
+  btn.innerText = "PARSE PROXIES";
+  btn.classList.add("btn", "button", "btn-primary");
+  btn.onclick = () => {
+    window.open(URL.createObjectURL(new Blob([parseNow()], { type: "text/html" })), "width=800,height=600");
+  };
+  document.body.appendChild(btn);
 })();
--- userscripts/sslproxies.js
+++ userscripts/sslproxies.js
@@ -15,21 +15,21 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
+  "use strict";
   /**
    * @returns {Promise<any[]>}
    */
   const parse = () => {
     return new Promise((resolve) => {
       // Select all table elements on the page
-      const tables = Array.from(document.querySelectorAll('table'));
+      const tables = Array.from(document.querySelectorAll("table"));
       const ipOnly = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/gm;
       const objectWrapper = [];
 
       // Loop through each table element using a for loop
       for (let i = 0; i < tables.length; i++) {
         const table = tables[i];
-        const rows = Array.from(table.querySelectorAll('tr'));
+        const rows = Array.from(table.querySelectorAll("tr"));
 
         for (let j = 0; j < rows.length; j++) {
           const row = rows[j];
@@ -40,10 +40,10 @@
             ssl: null,
             google: null,
             alert: null,
-            type: 'http',
+            type: "http",
             test: null
           };
-          const td = row.querySelectorAll('td');
+          const td = row.querySelectorAll("td");
           const proxy = td[0];
           const port = td[1];
           const countryCode = td[2];
@@ -57,15 +57,15 @@
             buildObject.ssl = /^yes/.test(ssl.innerText.trim()) ? true : false;
             buildObject.code = countryCode.innerText.trim();
             switch (anonymity.innerText.trim()) {
-              case 'elite proxy':
-                buildObject.anonymity = 'H';
+              case "elite proxy":
+                buildObject.anonymity = "H";
                 break;
-              case 'anonymous':
-                buildObject.anonymity = 'A';
+              case "anonymous":
+                buildObject.anonymity = "A";
                 break;
 
               default:
-                buildObject.anonymity = 'N';
+                buildObject.anonymity = "N";
                 break;
             }
             objectWrapper.push(buildObject);
@@ -76,14 +76,15 @@
     });
   };
 
-  const btn = document.createElement('button');
-  btn.setAttribute('style', 'position: fixed; bottom: 50%; right: 2em; transform: translateY(50%);');
-  btn.innerText = 'PARSE PROXIES';
-  btn.classList.add('btn', 'button', 'btn-primary');
+  const btn = document.createElement("button");
+  btn.id = "php-proxy-hunter-grab-proxy";
+  btn.setAttribute("style", "position: fixed; bottom: 50%; right: 2em; transform: translateY(50%);");
+  btn.innerText = "PARSE PROXIES";
+  btn.classList.add("btn", "button", "btn-primary");
   btn.onclick = () => {
     parse().then((result) => {
-      const html = result.map((obj) => JSON.stringify(obj)) + '<br/>';
-      window.open(URL.createObjectURL(new Blob([html], { type: 'text/html' })), 'width=800,height=600');
+      const html = result.map((obj) => JSON.stringify(obj)) + "<br/>";
+      window.open(URL.createObjectURL(new Blob([html], { type: "text/html" })), "width=800,height=600");
     });
   };
   document.body.appendChild(btn);
