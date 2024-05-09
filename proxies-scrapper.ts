3341563014dec6dc70c5647e0b4c0514cfb2454b
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-09 08:08:46 +0700

    node: update yarn and proxies-grabber

--- proxies-scrapper.ts
+++ proxies-scrapper.ts
@@ -1,6 +1,31 @@
-import proxyGrabber from "proxies-grabber";
+import axios from "axios";
+import { proxyGrabber } from "proxies-grabber";
 
 const grabber = new proxyGrabber();
-grabber.get().then(function(proxies) {
-  console.log(proxies);
+grabber.get().then(function (proxies) {
+  const result = JSON.stringify(proxies);
+  // Custom cookies
+  const cookies = {
+    __ga: "value_of__ga_cookie",
+    _ga: "value_of__ga_cookie"
+  };
+
+  // Convert cookies object to string
+  const cookieString = Object.entries(cookies)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  axios
+    .post("http://sh.webmanajemen.com/proxyAdd.php", new URLSearchParams({ proxies: result }), {
+      withCredentials: true,
+      headers: {
+        Cookie: cookieString
+      }
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch(() => {
+      //
+    });
 });
