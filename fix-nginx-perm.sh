3e9c30df70105e6303726a132c08fb5ff3447681
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-05-12 04:13:38 +0700

    drop sudo

--- fix-nginx-perm.sh
+++ fix-nginx-perm.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-# shellcheck disable=SC2035
+# Check if user is root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root"
+    exit 1
+fi
 
 # Array of files to remove
 lock_files=("proxyWorking.lock" "proxyChecker.lock")
@@ -17,69 +21,74 @@ for file in "${lock_files[@]}"; do
     fi
 done
 
+# Set permissions
+chmod 777 *.txt
+chmod 755 *.html *.js
+chmod 755 *.css
+chmod 755 js/*.js
+chmod 777 config
+chmod 755 config/*
+chmod 777 tmp .cache data
+chmod 644 data/*
+chmod 644 *.php
+chmod 644 .env
+
+# Create necessary directories and files
 mkdir -p tmp/cookies
 touch tmp/cookies/index.html
 touch tmp/index.html
 mkdir -p config
 touch config/index.html
 
-sudo chmod 777 *.txt
-sudo chmod 755 *.html
-sudo chmod 755 *.js
-sudo chmod 755 *.css
-sudo chmod 755 js/*.js
-sudo chmod 777 config
-sudo chmod 755 config/*
-sudo chmod 777 tmp
-sudo chmod 777 .cache
-sudo chmod 777 data
-sudo chmod 644 data/*
-sudo chmod 644 *.php
-sudo chmod 644 .env
-
+# Additional permissions for specific directories
 if [ -d "assets/proxies" ]; then
-    sudo chmod 755 "assets/proxies"
+    chmod 755 "assets/proxies"
     touch "assets/proxies/index.html"
 fi
 
-# to allow composer and indexing proxies work
-sudo chown -R www-data:www-data *.php *.phar
+# Allow composer and indexing proxies to work
+chown -R www-data:www-data *.php *.phar
 
-echo "permission sets successful"
+echo "Permission sets successful"
 
 OUTPUT_FILE="/var/www/html/proxyChecker.txt"
 COMPOSER_LOCK="/var/www/html/composer.lock"
 COMPOSER_PHAR="/var/www/html/composer.phar"
 
+# Install or update composer packages
 if [ ! -f "$COMPOSER_LOCK" ]; then
   su -s /bin/sh -c "php $COMPOSER_PHAR install >> $OUTPUT_FILE 2>&1" www-data
 else
   su -s /bin/sh -c "php $COMPOSER_PHAR update >> $OUTPUT_FILE 2>&1" www-data
 fi
 
-# validate proxies-all.php not running before indexing proxies
+# Validate proxies-all.php not running before indexing proxies
 if pgrep -f "proxies-all.php" >/dev/null; then
-    echo "proxies indexing is still running."
+    echo "Proxies indexing is still running."
 else
     # If proxies-all.php is not running, execute the command
     su -s /bin/sh -c "php /var/www/html/proxies-all.php >> $OUTPUT_FILE 2>&1 &" www-data
 fi
 
+# Set permissions for vendor directory
 chmod 777 vendor
 touch vendor/index.html
 
-echo "composer installed"
+echo "Composer installed"
 
-sudo chown -R www-data:www-data *.php *.txt *.json *.js *.html src data tmp vendor assets
-sudo chown -R www-data:www-data .cache config *.css *.lock js .htaccess .env
+# Fix ownership
+chown -R www-data:www-data *.php *.txt *.json *.js *.html src data tmp vendor assets
+chown -R www-data:www-data .cache config *.css *.lock js .htaccess .env
 
-echo "ownership fixed"
+echo "Ownership fixed"
 
-#git lfs install
-#git lfs track *.rar
+# Enable Git LFS and track large files
+git lfs install
+git lfs track *.rar
 
-echo "large files tracked"
+echo "Large files tracked"
 
+# Restart services
 systemctl restart php7.2-fpm
 systemctl restart nginx
 
