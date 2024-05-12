eaad804f92e6ea6a0aae963f5977ff14f7fca0b1
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-05-12 23:10:03 +0700

    remove duplicate resource

--- proxyFetcher.php
+++ proxyFetcher.php
@@ -37,7 +37,6 @@ $urls = array_unique([
     "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt",
     "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt",
     "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
-    "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
     "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
     "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
     "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
