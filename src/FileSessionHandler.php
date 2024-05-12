cfba20bde3c71e9f0f4e72f617cf748956e01329
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-12 18:06:02 +0700

    try modify session handler

--- src/FileSessionHandler.php
+++ src/FileSessionHandler.php
@@ -12,9 +12,15 @@ class FileSessionHandler implements SessionHandlerInterface
   protected $postfix;
 
   /**
+   * FileSessionHandler constructor.
+   *
+   * @param string $sess_path The path to the directory where session files will be stored.
+   * @param string $prefix (Optional) The prefix to prepend to session file names. Default is 'sess_'.
+   * @param string $postfix (Optional) The postfix to append to session file names.
+   *
    * @throws Exception
    */
-  public function __construct($sess_path, $prefix = 'sess_', $postfix = '')
+  public function __construct(string $sess_path, string $prefix = 'sess_', string $postfix = '')
   {
     if (!is_dir($sess_path)) {
       throw new Exception("Cannot use FileSessionHandler, directory '{$sess_path}' not found", 1);
@@ -29,45 +35,102 @@ class FileSessionHandler implements SessionHandlerInterface
     $this->postfix = $postfix;
   }
 
+  /**
+   * Initialize session.
+   *
+   * @param string $path
+   * @param string $name
+   * @return bool
+   */
   public function open($path, $name): bool
   {
     return true;
   }
 
+  /**
+   * Close the session.
+   *
+   * @return bool
+   */
   public function close(): bool
   {
     return true;
   }
 
+  /**
+   * Create file path for session.
+   *
+   * @param string $id
+   * @return string
+   */
+  private function createFilePath(string $id): string
+  {
+    // Sanitize session ID to prevent directory traversal attacks
+    $id = preg_replace('/[^a-zA-Z0-9_]/', '', $id);
+    return "{$this->sess_path}/{$this->prefix}{$id}{$this->postfix}";
+  }
+
+  /**
+   * Read session data.
+   *
+   * @param string $id
+   * @return string
+   */
   public function read($id): string
   {
-    return (string)@file_get_contents("{$this->sess_path}/{$this->prefix}{$id}{$this->postfix}");
+    $file = $this->createFilePath($id);
+    if (file_exists($file)) {
+      return (string)file_get_contents($file);
+    }
+    return '';
   }
 
+  /**
+   * Write session data.
+   *
+   * @param string $id
+   * @param string $data
+   * @return bool
+   */
   public function write($id, $data): bool
   {
-    return !(false === file_put_contents("{$this->sess_path}/{$this->prefix}{$id}{$this->postfix}", $data));
+    $file = $this->createFilePath($id);
+    if (file_put_contents($file, $data) !== false) {
+      return true;
+    }
+    return false;
   }
 
+  /**
+   * Destroy a session.
+   *
+   * @param string $id
+   * @return bool
+   */
   public function destroy($id): bool
   {
-    $file = "{$this->sess_path}/{$this->prefix}{$id}{$this->postfix}";
-
-    if (file_exists($file)) {
-      unlink($file);
+    $file = $this->createFilePath($id);
+    if (file_exists($file) && !unlink($file)) {
+      return false;
     }
-
     return true;
   }
 
+  /**
+   * Perform session garbage collection.
+   *
+   * @param int $max_lifetime
+   * @return bool
+   */
   public function gc($max_lifetime): bool
   {
-    foreach (glob("{$this->sess_path}/{$this->prefix}*") as $file) {
-      if (filemtime($file) + $max_lifetime < time() && file_exists($file)) {
+    $globs = array_merge(glob("{$this->sess_path}/{$this->prefix}*"), glob("{$this->sess_path}/{$this->prefix}*{$this->postfix}"));
+    $now = time();
+    foreach ($globs as $file) {
+      if (file_exists($file) && (filemtime($file) + $max_lifetime < $now)) {
         unlink($file);
       }
     }
-
     return true;
   }
 }
