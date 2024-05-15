e121490de99ac9ac0a0a6447c6a9086a3ad84896
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-16 06:56:27 +0700

    add all proxies iterator test

---
+++ tests/iterate-all-proxies.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/../func-proxy.php';
+
+use PhpProxyHunter\ProxyDB;
+
+$db = new ProxyDB(__DIR__ . '/../src/database.sqlite');
+$db->iterateAllProxies(function ($item) {
+//  echo $item['proxy'] . ' is valid ' . (isValidProxy($item['proxy']) ? 'true' : 'false') . PHP_EOL;
+  if (!isValidProxy($item['proxy'])) {
+    echo $item['proxy'] . PHP_EOL;
+  }
+});
