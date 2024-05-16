13cf9da3207ccd48bf2f0866bdb2386a8428b5f2
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-16 09:25:22 +0700

    feat: proxy checker parallel

---
+++ bin/check-proxy-parallel.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+REM Number of instances to run in parallel
+set NUM_INSTANCES=5
+
+REM Loop to start multiple instances
+for /l %%i in (1, 1, %NUM_INSTANCES%) do (
+    start "Instance %%i" php proxyCheckerParallel.php
+)
