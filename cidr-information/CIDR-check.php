5dce0af9ace41bf35e0560b778c1670ad6770c91
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-17 08:54:18 +0700

    update port scanner

--- cidr-information/CIDR-check.php
+++ cidr-information/CIDR-check.php
@@ -4,6 +4,9 @@
 
 require_once __DIR__ . '/../func-proxy.php';
 
+use \PhpProxyHunter\ProxyDB;
+use PhpProxyHunter\Scheduler;
+
 // disallow web server access
 if (php_sapi_name() !== 'cli') {
   // Redirect the user away or show an error message
@@ -28,82 +31,51 @@ function exitProcess()
   if (file_exists($lockFilePath)) unlink($lockFilePath);
   file_put_contents($statusFile, 'idle');
 }
+
 register_shutdown_function('exitProcess');
 
 // set memory
 ini_set('memory_limit', '2024M');
 
-$filePath = getRandomFileFromFolder(__DIR__ . '/tmp/ips-ports', 'txt');
-$outputPath = __DIR__ . '/proxies.txt';
-
-if (!is_file($filePath)) {
-  exit($filePath . ' is not file');
+$filePath = getRandomFileFromFolder(tmp() . '/ips-ports', 'txt');
+$outputPath = __DIR__ . '/../proxies.txt';
+$startTime = time();
+$str_to_remove = [];
+if (filesize($filePath) == 0) {
+  unlink($filePath);
+  exit("$filePath size 0");
 }
 
-// Open the file for reading
-$fileHandle = fopen($filePath, "r");
-
-// Check if the file opened successfully
-if ($fileHandle) {
-  // Open the output file for appending
-  $outputHandle = fopen($outputPath, "a"); // Changed mode to "a"
-
-  // Check if the output file opened successfully
-  if ($outputHandle) {
-    $startTime = time(); // Get the current timestamp
-
-    // Read the file line by line
-    while (($proxy = fgets($fileHandle)) !== false) {
-      // Check if the elapsed time is more than [n] seconds
+iterateBigFilesLineByLine([$filePath], 55, function (string $line) use (&$str_to_remove, $startTime, $filePath) {
+  $db = new ProxyDB();
+  $proxies = extractProxies($line, null, false);
+  for ($i = 0; $i < count($proxies); $i++) {
+    if (!is_debug()) {
       if (time() - $startTime > 300) {
         echo "Execution time exceeded. Stopping execution." . PHP_EOL;
-        // Break out of the loop
         break;
       }
-
-      $proxy = trim($proxy);
-
-      // dont process invalid IP:PORT
-      if (empty($proxy) || !isValidIPPort($proxy)) {
-        // Remove the proxy from the input file
-        removeStringFromFile($filePath, $proxy);
-        continue;
-      }
-
-      if (isPortOpen($proxy)) {
-        $http = checkProxy($proxy, 'http');
-        $socks5 = checkProxy($proxy, 'socks5');
-        $socks4 = checkProxy($proxy, 'socks4');
-        if ($http || $socks4 || $socks5) {
-          echo "$proxy working" . PHP_EOL;
-          // Write the proxy to the output file
-          fwrite($outputHandle, PHP_EOL . $proxy . PHP_EOL);
-          // Remove the proxy from the input file
-          removeStringFromFile($filePath, $proxy);
-        } else {
-          echo "$proxy port open, but not proxy" . PHP_EOL;
-          // Remove the proxy from the input file
-          removeStringFromFile($filePath, $proxy);
-        }
-      } else {
-        echo "$proxy port closed" . PHP_EOL;
-        // Remove the proxy from the input file
-        removeStringFromFile($filePath, $proxy);
-      }
     }
-
-    // Close the output file
-    fclose($outputHandle);
-  } else {
-    echo "Error: Unable to open output file!";
+    $item = $proxies[$i];
+    if (isPortOpen($item->proxy)) {
+      $db->updateData($item->proxy, ['status' => 'untested']);
+//      $http = checkProxy($item->proxy);
+//      $socks5 = checkProxy($item->proxy, 'socks5');
+//      $socks4 = checkProxy($item->proxy, 'socks4');
+//      if ($http || $socks4 || $socks5) {
+//        echo "$item->proxy working" . PHP_EOL;
+//      } else {
+//        echo "$item->proxy port open, but not proxy" . PHP_EOL;
+//      }
+    } else {
+      echo "$item->proxy port closed" . PHP_EOL;
+    }
+    $str_to_remove[] = $item->proxy;
+    Scheduler::register(function () use ($filePath, $str_to_remove) {
+      echo 'removing ' . count($str_to_remove) . ' lines' . PHP_EOL;
+      removeStringFromFile($filePath, $str_to_remove);
+    }, 'clean-up-' . basename(__FILE__));
   }
+});
 
-  // Close the input file
-  fclose($fileHandle);
-} else {
-  echo "Error: Unable to open input file!";
-}
 
-// filter IP:PORT only
-rewriteIpPortFile($outputPath);
-rewriteIpPortFile($filePath);
