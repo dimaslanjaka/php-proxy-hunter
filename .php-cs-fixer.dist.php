da2fab97e23293e229f23d04d46da0848c673244
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-23 10:50:35 +0700

    feat: php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+// composer exec php-cs-fixer fix
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__);
+
+$config = new Config();
+return $config->setRules([
+    '@PSR2' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'no_unused_imports' => true,
+])
+    ->setFinder($finder);
