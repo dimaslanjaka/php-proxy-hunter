2c597f8ce55eaf6506b2a346fe1fb0b4105ff065
dimaslanjaka <dimaslanjaka@gmail.com>
2024-05-29 07:36:24 +0700

    fix(buildCurl): remove Accept-Encoding header

--- tests/buildCurl.php
+++ tests/buildCurl.php
@@ -2,4 +2,6 @@
 
 require __DIR__ . '/../func-proxy.php';
 
-$ch = buildCurl(null, null, 'https://bing.com');
+$ch = buildCurl(null, null, 'https://www.google.com');
+$response = curl_exec($ch);
+var_dump($response);
