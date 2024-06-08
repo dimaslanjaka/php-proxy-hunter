af863bfdfa8eecaeee821078f9600b414771c9bb
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-06-08 01:03:31 +0000

    fix: keep original pem files

--- data/get-ca.sh
+++ data/get-ca.sh
@@ -9,17 +9,23 @@ create() {
   openssl s_client -showcerts -connect "$domain:443" </dev/null 2>/dev/null | openssl x509 -outform PEM >> "$SCRIPT_DIR/$domain.pem"
 }
 
-# Remove existing cacert.pem if it exists
-rm -f "$SCRIPT_DIR/cacert.pem"
+# Remove specific .pem files if they exist
+for file in "$SCRIPT_DIR"/*.pem; do
+  filename=$(basename "$file")
+  if [[ $filename != "cacert.pem" && $filename != "isrgrootx1.pem" && $filename != "curlhaxx_cacert.pem" ]]; then
+    rm "$file"
+  fi
+done
 
 # Download curl.pem from https://curl.haxx.se/ca/cacert.pem
-curl -L --output "$SCRIPT_DIR/curl.pem" "https://curl.haxx.se/ca/cacert.pem"
+curl -L --output "$SCRIPT_DIR/curlhaxx_cacert.pem" "https://curl.haxx.se/ca/cacert.pem"
 
 # Create PEM files for each domain
 create "www.google.com"
 create "www.instagram.com"
 create "www.facebook.com"
 create "translate.google.co.id"
+create "translate.google.com"
 create "api.facebook.com"
 create "graph.facebook.com"
 create "graph.beta.facebook.com"
@@ -28,3 +34,11 @@ create "facebook.com"
 
 # Concatenate all *.pem files into cacert.pem
 cat "$SCRIPT_DIR"/*.pem > "$SCRIPT_DIR/cacert.pem"
+
+# Remove all .pem files except specific ones
+for file in "$SCRIPT_DIR"/*.pem; do
+  filename=$(basename "$file")
+  if [[ $filename != "cacert.pem" && $filename != "isrgrootx1.pem" && $filename != "curlhaxx_cacert.pem" ]]; then
+    rm "$file"
+  fi
+done
