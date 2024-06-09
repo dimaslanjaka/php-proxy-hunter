b05124ea37fa90c74c7c5ffaf47e4fa9e33e5fcf
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-06-09 22:24:45 +0000

    feat: str_starts_with shimmer

--- func.php
+++ func.php
@@ -1131,6 +1131,21 @@ function moveLinesToFile(string $sourceFile, string $destinationFile, int $lines
   return true;
 }
 
+if (!function_exists('str_starts_with')) {
+  /**
+   * Checks if a string starts with a given prefix using regular expressions.
+   *
+   * @param string $haystack The input string.
+   * @param string $needle The prefix to check for.
+   * @return bool Returns true if the string starts with the prefix, false otherwise.
+   */
+  function str_starts_with(string $haystack, string $needle): bool
+  {
+    $pattern = '/^' . preg_quote($needle, '/') . '/';
+    return (bool) preg_match($pattern, $haystack);
+  }
+}
+
 /**
  * Append content to a file with file locking.
  *
