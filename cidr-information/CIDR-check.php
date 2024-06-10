48379a56e3d9b14047898c8d88e47350da0320c1
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-06-10 14:56:29 +0000

    change lock file

--- cidr-information/CIDR-check.php
+++ cidr-information/CIDR-check.php
@@ -7,15 +7,31 @@ require_once __DIR__ . '/../func-proxy.php';
 use \PhpProxyHunter\ProxyDB;
 use PhpProxyHunter\Scheduler;
 
+global $isCli;
+
 // disallow web server access
-if (php_sapi_name() !== 'cli') {
+if (!$isCli) {
   // Redirect the user away or show an error message
   header('HTTP/1.1 403 Forbidden');
   die('Direct access not allowed');
 }
 
-$lockFilePath = __DIR__ . "/proxyChecker.lock";
+$basename = basename(__FILE__, '.php');
+$lockFilePath = __DIR__ . "/tmp/$basename.lock";
 $statusFile = __DIR__ . "/status.txt";
+$short_opts = "p:m::";
+$long_opts = [
+  "proxy:",
+  "max::",
+  "userId::",
+  "lockFile::",
+  "runner::",
+  "admin::"
+];
+$options = getopt($short_opts, $long_opts);
+if (!empty($options['lockFile'])) {
+  $lockFilePath = $options['lockFile'];
+}
 
 if (file_exists($lockFilePath) && !is_debug()) {
   echo date(DATE_RFC3339) . ' another process still running' . PHP_EOL;
