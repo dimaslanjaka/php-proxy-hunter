71fa5b2bcd76c2deff9b701ebdd6474ca2e33865
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-06-10 16:51:01 +0000

    fix: execution with CLI

--- info.php
+++ info.php
@@ -2,22 +2,27 @@
 
 require_once __DIR__ . '/func-proxy.php';
 
-header('Content-Type: application/json; charset=utf-8');
+global $isCli;
 
-// modify config
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $input = parsePostData();
-  if (isset($input['config'])) {
-    $set = setConfig(getUserId(), $input['config']);
+if (!$isCli) {
+  header('Content-Type: application/json; charset=utf-8');
+
+  // modify config
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $input = parsePostData();
+    if (isset($input['config'])) {
+      $set = setConfig(getUserId(), $input['config']);
+    }
   }
+
+  if (isset($_REQUEST['txt'])) {
+    header('Content-Type: text/plain; charset=utf-8');
+  }
+
+  // get config
+  set_cookie("user_id", getUserId());
 }
 
-if (isset($_REQUEST['txt'])) {
-  header('Content-Type: text/plain; charset=utf-8');
-}
-
-// get config
-set_cookie("user_id", getUserId());
 $config = getConfig(getUserId());
 // admin info from 'data/login.php'
 $config['admin'] = isset($_SESSION['admin']) && $_SESSION['admin'] === true;
@@ -27,7 +32,7 @@ $config['captcha-site-key'] = $_ENV['G_RECAPTCHA_SITE_KEY'];
 $config['server-ip'] = getServerIp();
 $config_json = json_encode($config);
 
-set_cookie("user_config", base64_encode($config_json));
+if (!$isCli) set_cookie("user_config", base64_encode($config_json));
 
 echo $config_json;
 
