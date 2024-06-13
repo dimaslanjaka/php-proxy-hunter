0ee3efa210d5f654b6682fca4bf7611c7308b39e
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-06-13 23:36:26 +0000

    feat: github actions initializer

--- github-actions-init.php
+++ github-actions-init.php
@@ -14,7 +14,6 @@ $data = [
   "endpoint" => "https://www.example.com/",
   "headers" => [
     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Encoding: gzip, deflate, br",
     "Accept-Language: en-US,en;q=0.5",
     "Connection: keep-alive",
     "Host: www.example.com",
