c6368cb02b95bf6099d58efb0956770bbfc81112
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-06-13 23:35:57 +0000

    feat: github actions initializer

---
+++ github-actions-init.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/func-proxy.php';
+
+global $isCli;
+
+if (!$isCli) {
+  exit('Only CLI allowed');
+}
+
+// change config/CLI.json
+
+$data = [
+  "endpoint" => "https://www.example.com/",
+  "headers" => [
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding: gzip, deflate, br",
+    "Accept-Language: en-US,en;q=0.5",
+    "Connection: keep-alive",
+    "Host: www.example.com",
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"
+  ],
+  "type" => "http|socks4|socks5"
+];
+
+write_file(__DIR__ . '/config/CLI.json', json_encode($data));
