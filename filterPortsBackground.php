b4db4885a55d7caf6e1a5c8f347c2d38b14e84d9
dimaslanjaka <dimaslanjaka@gmail.com>
2024-06-20 08:09:36 +0700

    feat: run filter port duplicate

--- filterPortsBackground.php
+++ filterPortsBackground.php
@@ -22,41 +22,49 @@ if (!$isCli) {
 }
 
 // Run a long-running process in the background
-$file = __DIR__ . "/filterPorts.php";
-$output_file = __DIR__ . '/proxyChecker.txt';
-$pid_file = __DIR__ . '/filterPorts.pid';
-setMultiPermissions([$file, $output_file, $pid_file]);
+$files = [__DIR__ . "/filterPorts.php", __DIR__ . "/filterPortsDuplicate.php"];
+$lock_files = [];
 $isWin = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
-$cmd = "php " . escapeshellarg($file);
-if ($isWin) {
-  $cmd = "start /B \"filter_ports\" $cmd";
+
+foreach ($files as $file) {
+  $output_file = __DIR__ . '/proxyChecker.txt';
+  $pid_file = __DIR__ . '/tmp/' . md5($file) . '.pid';
+  $lock_files[] = $pid_file;
+  setMultiPermissions([$file, $output_file, $pid_file]);
+  $cmd = "php " . escapeshellarg($file);
+  if ($isWin) {
+    $window_name = md5($file);
+    $cmd = "start /B \"filter_ports_$window_name\" $cmd";
+  }
+
+  $uid = getUserId();
+  $cmd .= " --userId=" . escapeshellarg($uid);
+  $cmd .= " --max=" . escapeshellarg("30");
+  $cmd .= " --admin=" . escapeshellarg($isAdmin ? 'true' : 'false');
+
+  // validate lock files
+  $lock_file = __DIR__ . '/tmp/' . md5($file) . '.lock';
+  $lock_files[] = $lock_file;
+  if (file_exists($lock_file) && !is_debug() && !$isAdmin) {
+    exit(date(DATE_RFC3339) . ' another process still running' . PHP_EOL);
+  }
+
+  echo $cmd . "\n\n";
+
+  $cmd = sprintf("%s > %s 2>&1 & echo $! >> %s", $cmd, escapeshellarg($output_file), escapeshellarg($pid_file));
+
+  $runner = __DIR__ . "/tmp/runners/" . md5(__FILE__) . ($isWin ? '.bat' : "");
+  write_file($runner, $cmd);
+  setMultiPermissions($runner);
+
+  exec(escapeshellarg($runner));
 }
-
-$uid = getUserId();
-$cmd .= " --userId=" . escapeshellarg($uid);
-$cmd .= " --max=" . escapeshellarg("30");
-$cmd .= " --admin=" . escapeshellarg($isAdmin ? 'true' : 'false');
-
-// validate lock files
-if (file_exists(__DIR__ . '/proxyChecker.lock') && !is_debug() && !$isAdmin) {
-  exit(date(DATE_RFC3339) . ' another process still running' . PHP_EOL);
-}
-
-echo $cmd . "\n\n";
-
-$cmd = sprintf("%s > %s 2>&1 & echo $! >> %s", $cmd, escapeshellarg($output_file), escapeshellarg($pid_file));
-
-$runner = __DIR__ . "/tmp/runners/" . md5(__FILE__) . ($isWin ? '.bat' : "");
-write_file($runner, $cmd);
-setMultiPermissions($runner);
-
-exec(escapeshellarg($runner));
 
 function exitProcess()
 {
-  global $pid_file;
-  if (file_exists($pid_file)) {
-    unlink($pid_file);
+  global $lock_files;
+  foreach ($lock_files as $file) {
+    delete_path($file);
   }
 }
 register_shutdown_function('exitProcess');
