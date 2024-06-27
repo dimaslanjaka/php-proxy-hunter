fcafeacfa3623ad663e5ce2078e60038b95f1881
dimaslanjaka <dimaslanjaka@gmail.com>
2024-06-27 11:12:17 +0700

    feat: working site

--- userscripts/universal.user.js
+++ userscripts/universal.user.js
@@ -72,6 +72,7 @@
 // @match  			 *://freeproxylist.cc/*
 // @match  			 *://proxy-tools.com/*
 // @match  			 *://fineproxy.org/*
+// @match  			 *://proxy-spider.com/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
 // @downloadURL https://raw.githack.com/dimaslanjaka/php-proxy-hunter/master/userscripts/universal.user.js
