341233d5cdee31e5f95fd1e1c9688c61da4ae83b
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-05 06:07:41 +0700

    split proxies by 500 lines

--- proxyFetcher.ts
+++ proxyFetcher.ts
@@ -4,7 +4,7 @@ import { fs, path } from "sbg-utility";
 
 const grabber = new proxyGrabber();
 grabber.get().then(function (proxies) {
-  const result = JSON.stringify(proxies);
+  const result = JSON.stringify(proxies, null, 2);
   // Custom cookies
   const cookies = {
     __ga: "value_of__ga_cookie",
@@ -16,19 +16,41 @@ grabber.get().then(function (proxies) {
     .map(([key, value]) => `${key}=${value}`)
     .join("; ");
 
-  axios
-    .post("http://sh.webmanajemen.com/proxyAdd.php", new URLSearchParams({ proxies: result }), {
-      withCredentials: true,
-      headers: {
-        Cookie: cookieString
-      }
-    })
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch(() => {
-      //
-    });
+  const lines = splitStringByLines(result);
+  lines.forEach((line) => {
+    axios
+      .post("http://sh.webmanajemen.com/proxyAdd.php", new URLSearchParams({ proxies: line }), {
+        withCredentials: true,
+        headers: {
+          Cookie: cookieString
+        }
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch(() => {
+        //
+      });
+  });
 
   fs.appendFileSync(path.join(__dirname, "proxies.txt"), "\n" + result);
 });
+
+function splitStringByLines(inputString: string, linesPerChunk = 500) {
+  // Split the input string into an array of lines
+  const lines = inputString.split("\n");
+
+  // Initialize the result array
+  const chunks = [];
+
+  // Loop through the lines and create chunks
+  for (let i = 0; i < lines.length; i += linesPerChunk) {
+    // Get a chunk of the specified number of lines
+    const chunk = lines.slice(i, i + linesPerChunk);
+
+    // Join the chunk back into a string and push it to the chunks array
+    chunks.push(chunk.join("\n"));
+  }
+
+  return chunks;
+}
