1579b21d9111d23c00931ff4b920eef8bd54f1cf
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-06 06:20:43 +0700

    feat: full path php script feat: run filter ports duplicate

--- bin/check-proxy-parallel.cmd
+++ bin/check-proxy-parallel.cmd
@@ -3,7 +3,15 @@
 REM Number of instances to run in parallel
 set NUM_INSTANCES=5
 
+FOR %%i in ("%~dp0.") do SET "parent_folder=%%~fi"
+
+SET "cwd=%parent_folder%\..\"
+
+echo "CWD %cwd%"
+
 REM Loop to start multiple instances
 for /l %%i in (1, 1, %NUM_INSTANCES%) do (
-    start "Instance %%i" php proxyCheckerParallel.php --max=400
+    start "Check proxy %%i" php "%cwd%proxyCheckerParallel.php" --max=400
 )
+
+start "Filter ports" php "%cwd%filterPortsDuplicate.php"
