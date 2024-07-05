9eaaa4ae36a08a4d707cfe5aaad270f7b7ebca15
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-05 10:34:11 +0700

    feat: init chrome driver

---
+++ github-actions-init.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+# and if it doesn't exist, download it automatically,
+# then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()
+# Add your options as needed
+options = [
+    # Define window size here
+    "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+
+    # "--headless",
+    # "--disable-gpu",
+    # "--window-size=1920,1200",
+    # "--ignore-certificate-errors",
+    # "--disable-extensions",
+    # "--no-sandbox",
+    # "--disable-dev-shm-usage",
+    # '--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get('https://github.com')
+print(driver.title)
