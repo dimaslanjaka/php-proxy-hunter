f0237396425ac3bdd5fa0189b0927b97c7acb578
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-09 22:03:44 +0700

    feat: run script in bacgrkround helper (win or linux)

---
+++ tests/runScriptInbackground.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/../func-proxy.php';
+
+global $isCli, $isWin, $isAdmin;
+
+if (!$isCli) {
+  header('Content-Type: text/plain; charset=UTF-8');
+}
+if (!$isAdmin) exit('Access denied');
+
+$scriptPath = realpath(__DIR__ . '/../proxyChecker.py');
+$commandArgs = [
+  'max' => '5'
+];
+
+$result = runScriptInBackground($scriptPath, $commandArgs, "proxy-checker-python");
+
+if (isset($result['error'])) {
+  echo json_encode(['error' => $result['error']]);
+} else {
+  echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+}
