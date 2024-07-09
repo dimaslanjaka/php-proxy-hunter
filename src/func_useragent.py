6b876353654580f3dcb31b9bafa451406ba282c7
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-09 11:28:37 +0700

    fix: get pc useragent without selenium

--- src/func_useragent.py
+++ src/func_useragent.py
@@ -2,6 +2,7 @@ import os
 import random
 import tempfile
 from typing import Union, Optional
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as WebdriverOptions
 
@@ -21,23 +22,27 @@ def get_pc_useragent() -> Union[str, None]:
         result = read_file(cache_file)
         if result and result.strip():
             return result
-    driver: Optional[webdriver.Chrome] = None
-    try:
-        chrome_options = WebdriverOptions()
-        chrome_options.add_argument("--headless=new")
-        # chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        driver = webdriver.Chrome(options=chrome_options)
-        result = driver.execute_script("return navigator.userAgent")
-        if isinstance(result, str) and result.strip():
-            write_file(cache_file, result)
-    except Exception:
-        pass
-    finally:
-        if driver is not None:
-            driver.quit()
-    return result
+    # driver: Optional[webdriver.Chrome] = None
+    # try:
+    #     chrome_options = WebdriverOptions()
+    #     chrome_options.add_argument("--headless=new")
+    #     # chrome_options.add_argument("--disable-extensions")
+    #     chrome_options.add_argument("--disable-gpu")
+    #     chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    #     driver = webdriver.Chrome(options=chrome_options)
+    #     result = driver.execute_script("return navigator.userAgent")
+    #     if isinstance(result, str) and result.strip():
+    #         write_file(cache_file, result)
+    # except Exception:
+    #     pass
+    # finally:
+    #     if driver is not None:
+    #         driver.quit()
+    # return result
+    headers = requests.get("https://www.example.com").headers
+    user_agent = headers.get('User-Agent')
+    default_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    return user_agent or default_user_agent
 
 
 def random_windows_ua() -> str:
