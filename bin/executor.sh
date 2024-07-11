465ee3be786c050a40c89ea6dd1400397ba3f391
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-11 08:30:11 +0700

    update working executor (linux and windows cygwin)

--- bin/executor.sh
+++ bin/executor.sh
@@ -12,26 +12,15 @@ COMMAND="$@"
 # Set current working directory to parent directory of bin/
 CWD=$(dirname "$(dirname "$(realpath "$0")")")
 
-# Calculate MD5 hash of the command string
-HASH=$(echo -n "$COMMAND" | md5sum | cut -d ' ' -f 1)
-
-# Define output file path
-OUTPUT_FILE="$CWD/tmp/logs/$HASH.log"
-
-mkdir -p "$CWD/tmp/logs"
+echo "Current directory: $CWD"
+echo "Command: $COMMAND"
 
 # Check if OS is Linux and nginx is installed
 if [[ "$(uname)" == "Linux" && -x "$(command -v nginx)" ]]; then
   USER="www-data"
-  VENV_ACTIVATE="$CWD/venv/bin/activate"
-  # Activate the virtual environment
-  source "$VENV_ACTIVATE" # or . "$VENV_ACTIVATE"
-  su -s /bin/sh -c "$COMMAND" "$USER"
+  sudo -u "$USER" -H bash -c "source $CWD/venv/bin/activate && $COMMAND"
 else
-  VENV_ACTIVATE="$CWD/venv/Scripts/activate"
-  # Activate the virtual environment
-  source "$VENV_ACTIVATE" # or . "$VENV_ACTIVATE"
-  eval "$COMMAND"
+  eval "source $CWD/venv/Scripts/activate && $COMMAND"
 fi
 
-# bash bin/executor.sh python proxyChecker.py
+# Example usage: bash bin/executor.sh python proxyChecker.py
