7c6807d6b850cb730282ecd687694b54b3199cce
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-13 08:11:50 +0700

    drop `celery`

--- django_backend/apps/proxy/urls.py
+++ django_backend/apps/proxy/urls.py
@@ -3,9 +3,8 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from . import views
 
-# app_name = 'firstapp'
+app_name = 'proxy'
 urlpatterns = [
-    path('check/', views.trigger_check_proxy, name='check_proxy'),
-    path('test-celery/', views.test_celery, name='test_celery'),
-    path('', views.proxies_list, name='proxy_list')
+    path('', views.proxies_list, name='proxy_list'),
+    path('check', views.trigger_check_proxy, name='check_proxy'),
 ]
