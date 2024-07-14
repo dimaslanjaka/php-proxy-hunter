fc7b3cd57f5308c5bf974e6bc3a5c8a42e4ae8e7
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-14 07:22:32 +0700

    rename class

--- django_backend/apps/proxy/apps.py
+++ django_backend/apps/proxy/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyappConfig(AppConfig):
+class AppProxyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'django_backend.apps.proxy'
