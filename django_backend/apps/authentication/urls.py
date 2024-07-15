169c137aef4e6dc177610ecfbc9563745c61e70f
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-15 09:41:14 +0700

    feat: simple auth for development

--- django_backend/apps/authentication/urls.py
+++ django_backend/apps/authentication/urls.py
@@ -1,5 +1,9 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 from django.urls import path
 from . import views
+from src.func import is_debug
 
 urlpatterns = [
     path('create', views.CreateUserAPIView.as_view(), name='create-user'),
@@ -9,3 +13,8 @@ urlpatterns = [
     path('logout', views.LogoutUserAPIView.as_view(), name='logout-user'),
     path('status', views.CurrentUserStatusView.as_view(), name='current_user_status')
 ]
+
+if is_debug():
+    # add simple auth for development mode
+    urlpatterns.append(path('login/<username>/<password>', views.LoginUserAPIView.as_view(), name='login-user'))
+    urlpatterns.append(path('create/<username>/<password>', views.CreateUserAPIView.as_view(), name='create-user'))
