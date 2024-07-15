da20b1e7caeffeb304a09fd491ae8d2514afda0c
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-15 07:36:31 +0700

    feat: public static file

--- django_backend/apps/authentication/tests.py
+++ django_backend/apps/authentication/tests.py
@@ -28,6 +28,11 @@ class AdminLoginTests(TestCase):
             print(f'CSRF token {self.csrf_token}')
             self.headers.update({'X-CSRFToken': self.csrf_token})
 
+    def test_visit_homepage(self):
+        url = 'http://127.0.0.1:8000'
+        response = build_request(endpoint=url)
+        self.assertTrue(response.status_code == 200)
+
     def test_admin_login(self):
         # Ensure login page URL (adjust if using a different URL pattern)
         url = 'http://127.0.0.1:8000/auth/login'
