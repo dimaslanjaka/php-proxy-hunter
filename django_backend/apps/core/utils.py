6e894fdd7bea3b02c89c3297e87b6c112e3ed953
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-16 08:11:09 +0700

    feat: format rupiah

--- django_backend/apps/core/utils.py
+++ django_backend/apps/core/utils.py
@@ -1,7 +1,16 @@
+import locale
 import random
 import string
 
 DEFAULT_CHAR_STRING = string.ascii_lowercase + string.digits
+
+
+def rupiah_format(angka, with_prefix=False, desimal=2):
+    locale.setlocale(locale.LC_NUMERIC, 'IND')
+    rupiah = locale.format("%.*f", (desimal, angka), True)
+    if with_prefix:
+        return "Rp. {}".format(rupiah)
+    return rupiah
 
 
 def generate_random_string(chars=DEFAULT_CHAR_STRING, size=6):
