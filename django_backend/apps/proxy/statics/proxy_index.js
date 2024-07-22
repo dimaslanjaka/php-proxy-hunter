d30bbc7c61dbdfa6f722978e716d883e78c17e3d
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-22 15:26:22 +0700

    fill search input value using js

--- django_backend/apps/proxy/statics/proxy_index.js
+++ django_backend/apps/proxy/statics/proxy_index.js
@@ -1,4 +1,18 @@
-const searchInput = document.getElementById("searchInput");
-searchInput.addEventListener("keyup", () => {
-  searchInput.form.submit();
-});
+function init_search() {
+  const searchInput = document.getElementById("searchInput");
+  searchInput.addEventListener("keyup", () => {
+    searchInput.form.submit();
+  });
+  // Get the URL parameters
+  const urlParams = new URLSearchParams(window.location.search);
+
+  // Get the 'search' parameter
+  const searchParam = urlParams.get("search");
+
+  // If the 'search' parameter exists, set it as the value of the input field
+  if (searchParam !== null) {
+    searchInput.value = searchParam;
+  }
+}
+
+window.onload = init_search;
