117a3c63536ea81b6c9212c56943cb7245c227cc
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-23 08:41:44 +0700

    feat: file imports

--- packages/proxy-hunter-python/proxy_hunter/__init__.py
+++ packages/proxy-hunter-python/proxy_hunter/__init__.py
@@ -3,3 +3,4 @@ from .prox_check import is_prox
 from .extract_proxies import extract_proxies
 from .Proxy import Proxy, dict_to_proxy_list
 from .utils import is_valid_ip, is_valid_proxy, decompress_requests_response
+from . import utils, Proxy, extract_proxies, prox_check, proxyhunter
