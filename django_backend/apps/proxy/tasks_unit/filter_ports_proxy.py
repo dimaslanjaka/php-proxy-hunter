9463a9483b40b8fd45646f3f281b1982792afb25
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-25 22:16:35 +0700

    increase duplicate ips count to filter

--- django_backend/apps/proxy/tasks_unit/filter_ports_proxy.py
+++ django_backend/apps/proxy/tasks_unit/filter_ports_proxy.py
@@ -161,7 +161,7 @@ def filter_duplicates_ips(max: int = 10, callback: Optional[Callable] = None):
 
 def start_filter_duplicates_ips():
     # start checking [n] open ports from duplicated ips
-    thread = threading.Thread(target=filter_duplicates_ips, args=(10,))
+    thread = threading.Thread(target=filter_duplicates_ips, args=(1000,))
     thread.start()
     return thread
 
