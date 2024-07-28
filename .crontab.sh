0b28c2ff3174fc3f2da58c50c15d67719d267793
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-29 04:08:35 +0700

    update crontab tasks

--- .crontab.sh
+++ .crontab.sh
@@ -73,6 +73,7 @@ r_cmd() {
 
 # su -s /bin/sh -c "php $CWD/send_curl.php --url=https://dev.webmanajemen.com:8000/proxy/filter" "$USER"
 su -s /bin/sh -c "php $CWD/send_curl.php --url=https://dev.webmanajemen.com:8000/proxy/check" "$USER"
-bash "$CWD/bin/check-proxy-parallel"
-r_cmd "python" "filterPortsDuplicate.py" "--max=10"
-r_cmd "python" "proxyCheckerReal.py" "--max=10"
+# bash "$CWD/bin/check-proxy-parallel"
+# r_cmd "python" "filterPortsDuplicate.py" "--max=10"
+# r_cmd "python" "proxyCheckerReal.py" "--max=10"
+# djm check_proxies --max=10
