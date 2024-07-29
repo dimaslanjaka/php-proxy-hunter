af04e5da3e8ba84078befa638bdffd658727b3b4
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-29 13:16:04 +0700

    create virtual environment when not exist

--- bin/py-install.cmd
+++ bin/py-install.cmd
@@ -1,11 +1,38 @@
 @echo off
+setlocal
+
+REM Get the directory of the batch file
+set "SCRIPT_DIR=%~dp0"
+
+REM Remove the trailing backslash
+set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
+
+REM Move up two levels from the script directory
+for %%i in ("%SCRIPT_DIR%") do set "CWD=%%~dpi"
+set "CWD=%CWD:~0,-1%"
+
+REM Output the current working directory
+echo Working directory: %CWD%
+
+REM Check if the venv folder exists
+if not exist "%CWD%\venv" (
+    echo Creating virtual environment...
+    call pip install virtualenv
+    call python -m venv "%CWD%\venv"
+) else (
+    echo Virtual environment already exists.
+)
 
 :: Activate the virtual environment
-call venv\Scripts\activate
+call "%CWD%\venv\Scripts\activate"
 
-call python requirements_install.py
+REM Install requirements
+call python "%CWD%\requirements_install.py"
 
 :: Deactivate the virtual environment
-call venv\Scripts\deactivate
+call "%CWD%\venv\Scripts\deactivate"
 
-@REM D:\xampp\htdocs\venv\Scripts\python.exe -m pip install --upgrade pip
+@REM Uncomment the line below if you need to upgrade pip
+REM "%CWD%\venv\Scripts\python.exe" -m pip install --upgrade pip
+
+endlocal
