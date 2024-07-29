b2e3a811a10d31d13c95aae3a63c4df79e6b2af6
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-30 03:50:25 +0700

    feat: clear logs and show status

--- bin/clear-caches.sh
+++ bin/clear-caches.sh
@@ -5,3 +5,16 @@
 
 sync
 echo 3 > /proc/sys/vm/drop_caches
+
+# Clear old logs
+sudo du -sh /var/log/*
+sudo rm -f /var/log/*.gz
+
+# Clear trash
+rm -rf ~/.local/share/Trash/*
+
+# show usages
+echo "Disk Status:"
+df -h
+echo "RAM Status:"
+free -h
