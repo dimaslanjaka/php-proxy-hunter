ee644a4ccf25e5e3d7369d59efb253be9902e42b
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-29 13:16:04 +0700

    create virtual environment when not exist

--- bin/clean-install.cmd
+++ bin/clean-install.cmd
@@ -1,10 +1,34 @@
 @echo off
+setlocal
+
+REM Get the directory of the batch file
+set "SCRIPT_DIR=%~dp0"
+
+REM Remove the trailing backslash
+set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
+
+REM Move up two levels from the script directory
+for %%i in ("%SCRIPT_DIR%") do set "CWD=%%~dpi"
+set "CWD=%CWD:~0,-1%"
+
+REM Output the current working directory
+echo Working directory: %CWD%
+
+REM Check if the venv folder exists
+if not exist "%CWD%\venv" (
+    echo Creating virtual environment...
+    call pip install virtualenv
+    call python -m venv "%CWD%\venv"
+) else (
+    echo Virtual environment already exists.
+)
 
 :: Activate the virtual environment
-call D:\xampp\htdocs\venv\Scripts\activate
+call "%CWD%\venv\Scripts\activate"
 
 :: Clear Python cache
-for /r %%i in (__pycache__) do (
+echo Clearing Python cache...
+for /r "%CWD%" %%i in (__pycache__) do (
     if exist "%%i" (
         rd /s /q "%%i"
     )
@@ -16,22 +40,23 @@ python -m pip cache purge
 
 :: Freeze current packages to uninstall.txt
 echo Freezing current packages to uninstall.txt...
-python -m pip freeze > uninstall.txt
+python -m pip freeze > "%CWD%\uninstall.txt"
 
 :: Uninstall all packages listed in uninstall.txt
 echo Uninstalling all packages...
-python -m pip uninstall -r uninstall.txt -y
+python -m pip uninstall -r "%CWD%\uninstall.txt" -y
 
 :: Remove the uninstall.txt file
 echo Removing uninstall.txt...
-del uninstall.txt
+del "%CWD%\uninstall.txt"
 
 :: Reinstall packages from requirements.txt
 echo Reinstalling packages from requirements.txt...
-
-python requirements_install.py
+python "%CWD%\requirements_install.py"
 
 :: Deactivate the virtual environment
-deactivate
+call "%CWD%\venv\Scripts\deactivate"
 
 echo Done!
+
+endlocal
