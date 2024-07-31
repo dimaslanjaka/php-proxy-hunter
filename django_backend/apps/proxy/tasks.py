656c08606aebe99cb69fa37314cc717a08be4b71
dimaslanjaka <dimaslanjaka@gmail.com>
2024-07-31 17:42:17 +0700

    feat: working django background task using huey

--- django_backend/apps/proxy/tasks.py
+++ django_backend/apps/proxy/tasks.py
@@ -6,3 +6,4 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.
 from django_backend.apps.proxy.tasks_unit.filter_ports_proxy import *
 from django_backend.apps.proxy.tasks_unit.geolocation import *
 from django_backend.apps.proxy.tasks_unit.real_check_proxy import *
+from django_backend.apps.proxy.tasks_unit.huey import *
