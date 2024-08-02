235b62a2075eb8e227d341d4c61e7a1192389b07
dimaslanjaka <dimaslanjaka@gmail.com>
2024-08-02 10:13:07 +0700

    feat: clear journalctl logs

--- bin/clear-caches.sh
+++ bin/clear-caches.sh
@@ -9,6 +9,7 @@ echo 3 > /proc/sys/vm/drop_caches
 # Clear old logs
 sudo du -sh /var/log/*
 sudo rm -f /var/log/*.gz
+sudo journalctl --vacuum-time=1s
 
 # Clear trash
 rm -rf ~/.local/share/Trash/*
