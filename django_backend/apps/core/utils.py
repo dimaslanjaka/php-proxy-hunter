6ee9c5e05e4634c16333bc69c5a1cc1d243df71b
dimaslanjaka <dimaslanjaka@gmail.com>
2024-08-04 13:25:41 +0700

    feat(get_query_or_post_body): default value parameter

--- django_backend/apps/core/utils.py
+++ django_backend/apps/core/utils.py
@@ -4,6 +4,7 @@ import os
 import random
 import string
 import sys
+from typing import Any, Optional
 
 from django.http import HttpRequest, JsonResponse
 
@@ -24,10 +25,12 @@ def generate_random_string(chars=DEFAULT_CHAR_STRING, size=6):
     return "".join(random.choice(chars) for _ in range(size))
 
 
-def get_query_or_post_body(request: HttpRequest, key: str):
+def get_query_or_post_body(
+    request: HttpRequest, key: str, default_value: Optional[Any] = None
+):
     if request.method == "GET":
         # Get the proxy parameter from the query string
-        return request.GET.get(key, None)
+        return request.GET.get(key, default_value)
 
     elif request.method == "POST":
         content_type = request.content_type
@@ -36,11 +39,12 @@ def get_query_or_post_body(request: HttpRequest, key: str):
             try:
                 # Parse JSON data from the request body
                 data = json.loads(request.body)
-                return data.get(key, None)
+                return data.get(key, default_value)
             except json.JSONDecodeError:
                 # return JsonResponse({"error": "Invalid JSON"}, status=400)
                 pass
 
         elif content_type == "application/x-www-form-urlencoded":
             # Parse form data
-            return request.POST.get(key, None)
+            return request.POST.get(key, default_value)
+    return default_value
