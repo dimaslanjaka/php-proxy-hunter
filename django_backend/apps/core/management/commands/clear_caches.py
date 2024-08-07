bd190675e7216690437dceaafb30fecd1bec81b3
dimaslanjaka <dimaslanjaka@gmail.com>
2024-08-07 11:14:03 +0700

    fix: conflict with default clear cache command

---
+++ django_backend/apps/core/management/commands/clear_caches.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../"))
+SRC_DIR = os.path.join(BASE_DIR, "src")
+sys.path.append(SRC_DIR)
+from django.core.cache import cache
+from django.core.management.base import BaseCommand
+from src.func import delete_path, get_relative_path
+
+# python manage.py clear_caches
+
+
+class Command(BaseCommand):
+    help = "Clears the cache"
+
+    def handle(self, *args, **kwargs):
+        cache.clear()
+        delete_path(get_relative_path(".cache"))
+        self.stdout.write(self.style.SUCCESS("Cache cleared!"))
