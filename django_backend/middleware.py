93634fbd504e70362c037a97ef2aba885ba85f07
dimaslanjaka <dimaslanjaka@gmail.com>
2024-08-29 13:42:07 +0700

    drop django html minifier middleware

--- django_backend/middleware.py
+++ django_backend/middleware.py
@@ -14,7 +14,6 @@ from django.core.cache import cache
 from django.http import HttpRequest, HttpResponse
 from django.middleware.csrf import CsrfViewMiddleware
 from django.utils.deprecation import MiddlewareMixin
-from htmlmin.minify import html_minify
 from filelock import FileLock, Timeout
 
 
@@ -29,45 +28,6 @@ class CsrfExemptCsrfViewMiddleware(CsrfViewMiddleware):
         if request.headers.get("X-Greasemonkey-Script"):
             return None
         return super().process_view(request, callback, callback_args, callback_kwargs)
-
-
-class MinifyHTMLMiddleware(MiddlewareMixin):
-    def can_minify_response(self, request: HttpRequest, response: HttpResponse) -> bool:
-        result = "text/html" in response.get("Content-Type", "")
-
-        if hasattr(settings, "EXCLUDE_FROM_MINIFYING"):
-            for url_pattern in settings.EXCLUDE_FROM_MINIFYING:
-                regex = re.compile(url_pattern)
-                if regex.match(request.path.lstrip("/")):
-                    result = False
-                    break
-
-        return result
-
-    def process_response(
-        self, request: HttpRequest, response: HttpResponse
-    ) -> HttpResponse:
-        keep_comments = getattr(settings, "KEEP_COMMENTS_ON_MINIFYING", False)
-        parser = getattr(settings, "HTML_MIN_PARSER", "html5lib")
-        allowed = self.can_minify_response(request, response)
-
-        cache_key = f"minified_{request.get_full_path()}"
-        cached_response = cache.get(cache_key)
-
-        if cached_response and not settings.DEBUG:
-            # return cache only for production mode
-            response.content = cached_response
-        elif allowed and not settings.DEBUG:
-            minified_content = html_minify(
-                response.content, ignore_comments=not keep_comments, parser=parser
-            )
-            response.content = minified_content
-            response["Content-Length"] = len(response.content)
-            cache.set(
-                cache_key, minified_content, timeout=60 * 15
-            )  # Cache for 15 minutes
-
-        return response
 
 
 class FaviconMiddleware:
