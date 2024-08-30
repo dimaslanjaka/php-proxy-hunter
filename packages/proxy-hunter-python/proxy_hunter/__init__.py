a9a41da21b30bd9c749dec74717138ab3ee9b1f8
dimaslanjaka <dimaslanjaka@gmail.com>
2024-08-30 12:30:42 +0700

    move proxy extractor from file to package `proxy_hunter`

--- packages/proxy-hunter-python/proxy_hunter/__init__.py
+++ packages/proxy-hunter-python/proxy_hunter/__init__.py
@@ -1,6 +1,6 @@
 from .proxyhunter import scan, target
 from .prox_check import is_prox
-from .extract_proxies import extract_proxies
+from .extract_proxies import extract_proxies, extract_proxies_from_file
 from .Proxy import Proxy, dict_to_proxy_list
 from .utils import (
     is_valid_ip,
