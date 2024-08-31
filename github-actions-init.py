690f9c1294dcbd6e4cbd48c468be1e99070846fe
dimaslanjaka <dimaslanjaka@gmail.com>
2024-08-31 09:26:18 +0700

    remove unused codes

--- github-actions-init.py
+++ github-actions-init.py
@@ -1,7 +1,5 @@
 import os
 import stat
-import chromedriver_autoinstaller
-from selenium import webdriver
 
 
 def make_dirs_and_set_permissions():
@@ -18,33 +16,3 @@ def make_dirs_and_set_permissions():
 
 
 make_dirs_and_set_permissions()
-
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-# and if it doesn't exist, download it automatically,
-# then add chromedriver to path
-
-chrome_options = webdriver.ChromeOptions()
-# Add your options as needed
-options = [
-    # Define window size here
-    "--window-size=1200,1200",
-    "--ignore-certificate-errors",
-    # "--headless",
-    # "--disable-gpu",
-    # "--window-size=1920,1200",
-    # "--ignore-certificate-errors",
-    # "--disable-extensions",
-    # "--no-sandbox",
-    # "--disable-dev-shm-usage",
-    # '--remote-debugging-port=9222'
-]
-
-for option in options:
-    chrome_options.add_argument(option)
-
-
-driver = webdriver.Chrome(options=chrome_options)
-
-driver.get("https://github.com")
-print(driver.title)
-driver.quit()
