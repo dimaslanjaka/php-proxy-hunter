3bc891f61011ee596eadc8289e2d0fbe62e60fed
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-01 17:56:30 +0700

    fix: github actions init python

--- github-actions-init.py
+++ github-actions-init.py
@@ -1,12 +1,10 @@
 import os
 import stat
-import chromedriver_autoinstaller
-from selenium import webdriver
 
 
 def make_dirs_and_set_permissions():
     # List of directories to create
-    dirs = [".cache", "tmp", "config", "assets/proxies", "tmp/runners", "tmp/cookies"]
+    dirs = [".cache", "config", "assets/proxies", "tmp/runners", "tmp/cookies"]
 
     # Create directories
     for dir_path in dirs:
@@ -18,33 +16,3 @@ def make_dirs_and_set_permissions():
 
 
 make_dirs_and_set_permissions()
-
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-# and if it doesn't exist, download it automatically,
-# then add chromedriver to path
-
-chrome_options = webdriver.ChromeOptions()
-# Add your options as needed
-options = [
-    # Define window size here
-    "--window-size=1200,1200",
-    "--ignore-certificate-errors",
-    # "--headless",
-    # "--disable-gpu",
-    # "--window-size=1920,1200",
-    # "--ignore-certificate-errors",
-    # "--disable-extensions",
-    # "--no-sandbox",
-    # "--disable-dev-shm-usage",
-    # '--remote-debugging-port=9222'
-]
-
-for option in options:
-    chrome_options.add_argument(option)
-
-
-driver = webdriver.Chrome(options=chrome_options)
-
-driver.get("https://github.com")
-print(driver.title)
-driver.quit()
