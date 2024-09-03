b5a552bd5e546b0863342ec68fb0f29493859d12
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-03 15:08:40 +0700

    fix: proxy checker

--- packages/proxy-hunter-python/proxy_hunter/prox_check.py
+++ packages/proxy-hunter-python/proxy_hunter/prox_check.py
@@ -1,31 +1,47 @@
-#!/usr/bin/env python3
 """ Proxy server checker for proxyscan.py """
 
+from typing import Optional
 import requests
 
 
-def is_prox(proxy_server):
+def is_prox(proxy_server: str, debug: bool = False) -> Optional[str]:
+    """
+    Test if a given proxy server is working by sending a request to a test site.
+
+    Args:
+        proxy_server (str): The proxy server to test.
+
+    Returns:
+        Optional[str]: The proxy server if it is working, otherwise None.
+    """
     proxyDict = {"http": proxy_server, "https": proxy_server, "socks": proxy_server}
 
     test_site = "http://api.ipify.org/?format=json"
     headers = {
-        "user-agent": "Mozilla/"
-        "5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/"
-        "20091102 Firefox/"
-        "3.5.5 (.NET CLR 3.5.30729)"
+        "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)"
     }
 
-    for proxy in proxyDict:
+    for proxy_type, proxy in proxyDict.items():
+        proxies = {proxy_type: proxy}
         try:
-            r = requests.get(test_site, headers=headers, proxies=proxy)
-            status = r.status_code
-            if status == 200:
-                return proxy
+            response = requests.get(
+                test_site, headers=headers, proxies=proxies, verify=False
+            )
+            if response.ok:
+                status = response.status_code
+                if status == 200:
+                    return proxy
+                else:
+                    print(f"{proxy} got status {status}")
             else:
-                pass
+                print(f"{proxy} response not ok")
         except Exception as e:
-            pass
+            if debug:
+                print(f"{proxy} error {e}")
+
+    return None
 
 
 if __name__ == "__main__":
-    is_prox()
+    result = is_prox("51.15.190.163:14522", True)
+    print(result)
