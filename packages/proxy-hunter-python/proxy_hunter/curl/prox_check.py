4f290d028709da70c0cd9b2ddf5b09d19b07815d
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-05 05:29:02 +0700

    feat: add more url to test

--- packages/proxy-hunter-python/proxy_hunter/curl/prox_check.py
+++ packages/proxy-hunter-python/proxy_hunter/curl/prox_check.py
@@ -15,33 +15,37 @@ def is_prox(proxy_server: str, debug: bool = False) -> Optional[str]:
     Returns:
         Optional[str]: The proxy server if it is working, otherwise None.
     """
-    endpoint = "http://api.ipify.org/?format=json"
+    endpoints = [
+        "https://ip-get-geolocation.com/api/json",
+        "http://api.ipify.org/?format=json",
+    ]
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)"
     }
 
     proxy_types = ["http", "socks4", "socks5"]
 
-    for proxy_type in proxy_types:
-        format_proxy = f"{proxy_type}://{proxy_server}"
-        try:
-            response = build_request(
-                proxy=proxy_server,
-                proxy_type=proxy_type,
-                method="GET",
-                post_data=None,
-                endpoint=endpoint,
-                headers=headers,
-            )
-            if response and response.ok:
-                response_json = response.json()
-                if response_json.get("ip"):
-                    return proxy_server
-                elif debug:
-                    print(f"{format_proxy} failed: caused by decode json")
-        except Exception as e:
-            if debug:
-                print(f"{format_proxy} failed:", str(e))
+    for endpoint in endpoints:
+        for proxy_type in proxy_types:
+            format_proxy = f"{proxy_type}://{proxy_server}"
+            try:
+                response = build_request(
+                    proxy=proxy_server,
+                    proxy_type=proxy_type,
+                    method="GET",
+                    post_data=None,
+                    endpoint=endpoint,
+                    headers=headers,
+                )
+                if response and response.ok:
+                    response_json = response.json()
+                    if response_json.get("ip") or response_json.get("timezone"):
+                        return proxy_server
+                    elif debug:
+                        print(f"{format_proxy} failed: caused by decode json")
+            except Exception as e:
+                if debug:
+                    print(f"{format_proxy} failed:", str(e))
 
     return None
 
