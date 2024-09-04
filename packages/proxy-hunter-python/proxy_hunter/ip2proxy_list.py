b83e98047a3eeaeb58b7684b4025b590aa30030c
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-04 15:56:44 +0700

    fix: ip port pairs generator

--- packages/proxy-hunter-python/proxy_hunter/ip2proxy_list.py
+++ packages/proxy-hunter-python/proxy_hunter/ip2proxy_list.py
@@ -1,17 +1,25 @@
 from typing import List
 
 
-def generate_ip_port_pairs(ip: str, start_port: int = 80, max_port: int = 65535):
+from typing import List, Tuple
+
+
+def generate_ip_port_pairs(
+    ip: str, start_port: int = 80, max_port: int = 65535
+) -> List[Tuple[str, int]]:
     ip_parts = ip.split(".")
     if len(ip_parts) != 4:
         raise ValueError("Invalid IP address format")
 
     base_ip_parts = list(map(int, ip_parts))
-    pairs: List[str] = []
+    pairs: List[Tuple[str, int]] = []
+
+    base_ip = (
+        f"{base_ip_parts[0]}.{base_ip_parts[1]}.{base_ip_parts[2]}.{base_ip_parts[3]}"
+    )
 
     for port in range(start_port, max_port + 1):
-        ip = f"{base_ip_parts[0]}.{base_ip_parts[1]}.{base_ip_parts[2]}.{base_ip_parts[3]}"
-        pairs.append((ip, port))
+        pairs.append((base_ip, port))
 
     return pairs
 
