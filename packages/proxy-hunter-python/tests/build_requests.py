f5569118d3ae91cd7c2e83f1d0f51740dcc91086
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-05 05:01:03 +0700

    feat: test using proxy

--- packages/proxy-hunter-python/tests/build_requests.py
+++ packages/proxy-hunter-python/tests/build_requests.py
@@ -1,17 +1,47 @@
 import unittest
+import certifi
+from requests import Response
 
 from proxy_hunter.curl.request_helper import build_request
 
 
 class TestMyClass(unittest.TestCase):
 
-    def test_x(self):
+    def test_without_proxy(self):
         response = self.do_request()
         self.assertEqual(response.status_code, 200)
 
-    def do_request(self, proxy=None):
+    def test_with_proxy(self):
+        response = None
+        proxy = "117.40.32.135:8080"
         try:
-            return build_request(endpoint="https://www.example.com", proxy=proxy)
+            response = self.do_request(
+                proxy=proxy, proxy_type="http", verify=certifi.where()
+            )
+        except Exception:
+            pass
+        if not response:
+            try:
+                response = self.do_request(
+                    proxy=proxy, proxy_type="socks4", verify=certifi.where()
+                )
+            except Exception:
+                pass
+        if not response:
+            try:
+                response = self.do_request(
+                    proxy=proxy, proxy_type="socks5", verify=certifi.where()
+                )
+            except Exception:
+                pass
+        self.assertTrue(isinstance(response, Response))
+        if response:
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue("<title>Example Domain</title>" in response.text)
+
+    def do_request(self, **kwargs):
+        try:
+            return build_request(endpoint="https://www.example.com", **kwargs)
         except Exception:
             return None
 
