73335140d01bc2b6e4d37a8c4af1bba1debc4186
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-04 18:33:11 +0700

    move `build_requests` into `proxy_hunter`

--- django_backend/apps/authentication/test_unit/user.py
+++ django_backend/apps/authentication/test_unit/user.py
@@ -1,7 +1,10 @@
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../"))
+)
 from .admin import *
 
 # create user by CLI
@@ -17,41 +20,61 @@ class TestUser(unittest.TestCase):
         cls.admin.test_2_login()
         cls.headers = cls.admin.headers
         cls.login_data = {
-            'username': 'demo',
-            'password': 'pass',
-            'email': 'no-reply@blogger.com'
+            "username": "demo",
+            "password": "pass",
+            "email": "no-reply@blogger.com",
         }
 
     def test_1_create(self):
-        url = 'http://127.0.0.1:8000/auth/create'
-        self.headers.update({
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'X-CSRFToken': utils.parse_csrf_token_from_cookie_file(self.admin.cookie_file)
-        })
-        response = build_request(endpoint=url, headers=self.headers, post_data=json.dumps(self.login_data), method='POST', cookie_file=self.admin.cookie_file)
+        url = "http://127.0.0.1:8000/auth/create"
+        self.headers.update(
+            {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "X-CSRFToken": utils.parse_csrf_token_from_cookie_file(
+                    self.admin.cookie_file
+                ),
+            }
+        )
+        response = build_request(
+            endpoint=url,
+            headers=self.headers,
+            post_data=json.dumps(self.login_data),
+            method="POST",
+            cookie_file=self.admin.cookie_file,
+        )
         if response.status_code != 201 and response.status_code != 304:
-            print('test_1_create', response.text, response.status_code)
+            print("test_1_create", response.text, response.status_code)
         self.assertTrue(response.status_code == 201 or response.status_code == 304)
-        self.assertTrue('created success' in response.text)
+        self.assertTrue("created success" in response.text)
 
     def test_2_login(self):
         self.admin.test_4_logout()
-        url = 'http://127.0.0.1:8000/auth/login'
-        response = build_request(endpoint=url, headers=self.headers, post_data=json.dumps(self.login_data), method='POST', cookie_file=self.admin.cookie_file)
+        url = "http://127.0.0.1:8000/auth/login"
+        response = build_request(
+            endpoint=url,
+            headers=self.headers,
+            post_data=json.dumps(self.login_data),
+            method="POST",
+            cookie_file=self.admin.cookie_file,
+        )
         if response.status_code != 200:
-            print('test_2_login', response.text)
+            print("test_2_login", response.text)
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Login success" in response.text)
 
     def test_4_logout(self):
-        url = 'http://127.0.0.1:8000/auth/logout'
-        response = build_request(endpoint=url, cookie_file=self.admin.cookie_file, headers=self.headers)
-        self.assertFalse('HTTP 401 Unauthorized' in response.text)
-        self.assertTrue('Logout success' in response.text)
+        url = "http://127.0.0.1:8000/auth/logout"
+        response = build_request(
+            endpoint=url, cookie_file=self.admin.cookie_file, headers=self.headers
+        )
+        self.assertFalse("HTTP 401 Unauthorized" in response.text)
+        self.assertTrue("Logout success" in response.text)
 
     def test_5_delete(self):
         self.admin.test_2_login()
         url = f"http://127.0.0.1:8000/auth/delete/{self.login_data['username']}"
-        response = build_request(endpoint=url, cookie_file=self.admin.cookie_file, headers=self.headers)
-        self.assertTrue('deleted success' in response.text)
+        response = build_request(
+            endpoint=url, cookie_file=self.admin.cookie_file, headers=self.headers
+        )
+        self.assertTrue("deleted success" in response.text)
