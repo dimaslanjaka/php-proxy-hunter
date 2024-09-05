2655e42acdad7af6306390f0cb19343f8f4e726e
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-05 17:53:12 +0700

    feat: filter duplicated proxies

--- .crontab.sh
+++ .crontab.sh
@@ -115,6 +115,7 @@ fi
 if should_run_job "tmp/crontab/1-h" 1; then
   php "$CWD/send_curl.php" --url=https://sh.webmanajemen.com:8443/proxy/check
   djm check_proxies --max=100
+  djm filter_dups --max=100
 fi
 
 # run every 3 hours
