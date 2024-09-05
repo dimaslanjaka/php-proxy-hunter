af0d37173a356ebbb67288c020147d1c85299886
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-06 06:19:30 +0700

    move regex utils into `proxy_hunter`

--- src/func.py
+++ src/func.py
@@ -262,38 +262,6 @@ def is_directory_created_days_ago_or_more(directory_path: str, days: int) -> boo
         return False
 
 
-def is_matching_regex(pattern: str, text: str):
-    """
-    Check if the given text matches the provided regular expression pattern.
-
-    Args:
-    pattern (str): The regular expression pattern to match against.
-    text (str): The text to check for a match.
-
-    Returns:
-    bool: True if the text matches the pattern, False otherwise.
-    """
-    # return bool(re.match(pattern, text))
-    return re.search(pattern, text, re.MULTILINE) is not None
-
-
-def find_substring_from_regex(pattern: str, string: str) -> Optional[str]:
-    """
-    Find and return the first substring in the given string that matches the specified regex pattern.
-
-    Args:
-        pattern (str): The regex pattern to search for.
-        string (str): The string to search in.
-
-    Returns:
-        str: The matched substring, or None if no match is found.
-    """
-    matches = re.finditer(pattern, string, re.MULTILINE)
-    for match in matches:
-        return match.group()
-    return None
-
-
 def keep_alphanumeric_and_remove_spaces(input_string: str) -> str:
     """
     Removes spaces and keeps only alphanumeric characters from the input string.
