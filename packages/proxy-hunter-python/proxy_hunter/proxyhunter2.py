e6db95f129dcec93b175a0eaea889003ff203d79
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-05 17:14:55 +0700

    using thread

--- packages/proxy-hunter-python/proxy_hunter/proxyhunter2.py
+++ packages/proxy-hunter-python/proxy_hunter/proxyhunter2.py
@@ -1,15 +1,14 @@
 import os
-from typing import Tuple
+import threading
+from typing import List, Tuple
 
 from proxy_hunter.cidr2ips import list_ips_from_cidr
 from proxy_hunter.curl.prox_check import is_prox
 from proxy_hunter.ip2cidr import calculate_cidr
 from proxy_hunter.ip2proxy_list import generate_ip_port_pairs
 from proxy_hunter.ip2subnet import get_default_subnet_mask
-from proxy_hunter.utils import IterationHelper
 from proxy_hunter.utils.file import (
     delete_path,
-    write_json,
     load_tuple_from_file,
     save_tuple_to_file,
 )
@@ -36,17 +35,21 @@ def proxy_hunter2(proxy: str):
         proxies = [pair for ip in ips for pair in generate_ip_port_pairs(ip)]
         save_tuple_to_file(cache_file, proxies)
 
+    threads: List[threading.Thread] = []
+
     def callback(proxy: Tuple[str, int]):
-        nonlocal proxies
+        nonlocal proxies, threads
         ip, port = proxy
         proxy_str = f"{ip}:{port}"
         check = is_prox(proxy_str)
-        print("{} is proxy {}".format(proxy_str, str(check)))
+        print("{} {}\t".format(proxy_str, "is proxy" if check is not None else "is not proxy"))
         proxies = [item for item in proxies if item != proxy]
         save_tuple_to_file(cache_file, proxies)
 
-    iterator = IterationHelper(proxies, callback, iter_data)
-    iterator.run()
+    for item in proxies:
+        thread = threading.Thread(target=callback, args=(item,))
+        threads.append(thread)
+        thread.start()
 
 
 if __name__ == "__main__":
