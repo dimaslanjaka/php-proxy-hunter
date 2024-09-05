9cec6438450643e4142ee83eb5419c078755736f
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-05 08:48:40 +0700

    fix: auto fix .husky/hash.txt while merging

--- bin/create-file-hashes.cmd
+++ bin/create-file-hashes.cmd
@@ -10,15 +10,21 @@ if exist .env (
 )
 
 rem Get the directory of the current script
-set SCRIPT_DIR=%~dp0
-rem Remove trailing backslash
-set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+set "SCRIPT_DIR=%~dp0"
 echo Current script directory: %SCRIPT_DIR%
 
-REM Move up two levels from the script directory
-for %%i in ("%SCRIPT_DIR%") do set "CWD=%%~dpi"
+rem Remove trailing backslash if present
+if "%SCRIPT_DIR:~-1%"=="\" set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
+
+rem Move up one level from the script directory
+for %%i in ("%SCRIPT_DIR%..") do set "CWD=%%~dpi"
 set "CWD=%CWD:~0,-1%"
 
-node "%SCRIPT_DIR%create-file-hashes.js"
+rem Configure the custom merge driver
+git config merge.ourhashdriver.name "Custom Hash File Merge Driver"
+git config merge.ourhashdriver.driver "node \"%SCRIPT_DIR%\create-file-hashes.js\" %A"
+
+REM Run the script
+node "%SCRIPT_DIR%\create-file-hashes.js"
 
 endlocal
