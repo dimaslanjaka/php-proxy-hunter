68bdd52a464892b9c19539d3121b363a8d409b9b
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-08 15:23:00 +0700

    optimize codes

--- github-actions-init.py
+++ github-actions-init.py
@@ -1,15 +1,30 @@
 import os
+import platform
 import shutil
 import stat
 import subprocess
 import sys
-
-# List of packages to check
-packages = ["pycountry", "django", "PySide6", "requests", "pytz", "timezonefinder"]
+from typing import List
 
 
-# Function to check if a package is installed
-def check_package(package_name):
+def run_command(command: List[str]):
+    try:
+        result = subprocess.run(
+            command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        return result.stdout.decode("utf-8").strip()
+    except subprocess.CalledProcessError:
+        return None
+
+
+def is_git_repo_and_on_branch(branch_name: str = "python"):
+    return (
+        run_command(["git", "rev-parse", "--is-inside-work-tree"]) is not None
+        and run_command(["git", "branch", "--show-current"]) == branch_name
+    )
+
+
+def check_package(package_name: str):
     try:
         __import__(package_name)
         return True
@@ -17,43 +32,46 @@ def check_package(package_name):
         return False
 
 
-# Check if all packages are installed
-missing_packages = [pkg for pkg in packages if not check_package(pkg)]
+def delete_path(path: str) -> None:
+    if os.path.exists(path):
+        try:
+            if os.path.isdir(path):
+                shutil.rmtree(path, ignore_errors=True)
+            elif os.path.isfile(path):
+                os.remove(path)
+            print(f"'{path}' deleted successfully.")
+        except OSError as e:
+            print(f"Error deleting '{path}': {e}")
+    else:
+        print(f"Path '{path}' does not exist.")
 
+
+def ensure_directories(directories: List[str]):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    for dir_path in directories:
+        full_path = os.path.join(current_directory, dir_path)
+        os.makedirs(full_path, exist_ok=True)
+        os.chmod(full_path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+
+
+# List of packages to check
+packages = ["pycountry", "django", "requests", "pytz", "timezonefinder"]
+
+# Add Windows-specific packages if needed
+if platform.system() == "Windows" and is_git_repo_and_on_branch("python"):
+    packages.extend(["PySide6", "psutil", "wmi", "qtawesome", "nuitka"])
+
+# Check for missing packages
+missing_packages = [pkg for pkg in packages if not check_package(pkg)]
 if missing_packages:
     print(f"Missing packages: {', '.join(missing_packages)}")
-    # Run pip install to install from requirements.txt
     subprocess.check_call(
         [sys.executable, "-m", "pip", "install", "-r", "requirements.txt"]
     )
 else:
     print("All required packages are installed.")
 
-
-def delete_path(path: str) -> None:
-    """
-    Delete a folder or file specified by the path if it exists.
-
-    Args:
-        path (str): The path of the folder or file to delete.
-    """
-    if not os.path.exists(path):
-        print(f"Path '{path}' does not exist.")
-        return
-
-    try:
-        if os.path.isdir(path):
-            shutil.rmtree(path, ignore_errors=True)
-            print(f"Folder '{path}' and its contents deleted successfully.")
-        elif os.path.isfile(path):
-            os.remove(path)
-            print(f"File '{path}' deleted successfully.")
-        else:
-            print(f"Path '{path}' is neither a file nor a folder.")
-    except OSError as e:
-        print(f"Error deleting '{path}': {e}")
-
-
+# Define directories
 temp_dirs = ["tmp/runners", "tmp/logs"]
 dirs = [
     ".cache",
@@ -67,15 +85,8 @@ dirs = [
     "dist",
 ] + temp_dirs
 
-# Delete directories contents
-for dir_path in temp_dirs:
-    if os.path.exists(dir_path):
-        delete_path(dir_path)
+# Clean temporary directories and recreate all necessary directories
+for temp_dir in temp_dirs:
+    delete_path(temp_dir)
 
-# Create directories
-for dir_path in dirs:
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    dir_path = os.path.join(current_directory, dir_path)
-    os.makedirs(dir_path, exist_ok=True)
-    # Set permissions to 777
-    os.chmod(dir_path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+ensure_directories(dirs)
