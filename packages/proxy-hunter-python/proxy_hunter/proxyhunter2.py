3ee065584e0e598a03273fdab3a3df42babca41d
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-09 13:50:21 +0700

    feat: register signal

--- packages/proxy-hunter-python/proxy_hunter/proxyhunter2.py
+++ packages/proxy-hunter-python/proxy_hunter/proxyhunter2.py
@@ -3,6 +3,7 @@ import concurrent.futures
 import os
 import random
 import re
+import signal
 import sys
 import threading
 from typing import Callable, Dict, List, Optional
@@ -11,11 +12,7 @@ from proxy_hunter.curl.prox_check import is_prox
 from proxy_hunter.curl.proxy_utils import is_port_open
 from proxy_hunter.extractor import extract_ips
 from proxy_hunter.ip2proxy_list import generate_ip_port_pairs
-from proxy_hunter.utils.file import (
-    read_file,
-    remove_string_from_file,
-    write_file,
-)
+from proxy_hunter.utils.file import read_file, remove_string_from_file, write_file
 
 # Define a global counter and lock for controlling output
 print_lock = threading.Lock()
@@ -68,7 +65,7 @@ def process_iterated_proxy(
     if callable(callback):
         callback(proxy, is_open, is_proxy)
     at_exit_data[ip].append(proxy)
-    remove_string_from_file(file, at_exit_data[ip], True)
+    remove_string_from_file(file, proxy, True)
 
 
 def iterate_gen_ports(
@@ -92,7 +89,7 @@ def iterate_gen_ports(
             f"Got {len(filtered_lines)} proxies extracted from {file}", end="\n"
         )
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
             futures = []
             for line_proxy in filtered_lines:
                 futures.append(
@@ -103,7 +100,7 @@ def iterate_gen_ports(
             concurrent.futures.wait(futures)
 
 
-def register_exit():
+def register_exit(signum=None, frame=None):
     global at_exit_data
     for ip, data in at_exit_data.items():
         file = f"tmp/ip-ports/{ip}.txt"
@@ -111,6 +108,8 @@ def register_exit():
 
 
 atexit.register(register_exit)
+signal.signal(signal.SIGTERM, register_exit)
+signal.signal(signal.SIGINT, register_exit)  # To handle Ctrl+C
 
 if __name__ == "__main__":
     proxy = "156.34.105.58:5678"
