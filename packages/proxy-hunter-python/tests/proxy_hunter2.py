eb8c4cacebaa56a46f189ce25f58c9e4d575cee8
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-09 13:52:20 +0700

    feat: proxy_hunter2

--- packages/proxy-hunter-python/tests/proxy_hunter2.py
+++ packages/proxy-hunter-python/tests/proxy_hunter2.py
@@ -1,6 +1,5 @@
-from proxy_hunter.proxyhunter2 import gen_ports, iterate_gen_ports
+from proxy_hunter.proxyhunter2 import proxy_hunter2
 
 if __name__ == "__main__":
     proxy = "156.34.105.58:5678"
-    gen_ports(proxy)
-    iterate_gen_ports(proxy)
+    proxy_hunter2(proxy)
