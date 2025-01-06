47f87520efe0ca4279a7ab504ceb9385aab199c4
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-06 08:09:34 +0700

    feat: test vacuum

--- tests/ProxyDB_Test.py
+++ tests/ProxyDB_Test.py
@@ -11,6 +11,15 @@ null = None
 
 class TestStringMethods(unittest.TestCase):
 
+    def test_vacuum(self):
+        self.db = ProxyDB()
+        self.db.db.execute_query("PRAGMA journal_mode = WAL")
+        self.db.db.execute_query("PRAGMA wal_autocheckpoint = 100")
+        self.db.db.execute_query("PRAGMA auto_vacuum = FULL")
+        self.db.db.execute_query("VACUUM")
+        # https://stackoverflow.com/a/37865221/6404439
+        self.db.db.execute_query("PRAGMA wal_checkpoint(SQLITE_CHECKPOINT_TRUNCATE);")
+
     def test_get_all(self):
         self.db = ProxyDB()
         all_p = self.db.get_all_proxies(True)[:10]
