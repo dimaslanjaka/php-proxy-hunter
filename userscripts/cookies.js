afcd798422fe7fb56704ebd176d60ff7fe036d71
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-08 18:09:45 +0700

    refactor: standardize string delimiters to single quotes in user scripts

--- userscripts/cookies.js
+++ userscripts/cookies.js
@@ -12,22 +12,22 @@
 })();
 
 function getAllCookies() {
-  var cookies = document.cookie.split(";"); // Split cookies into an array
+  var cookies = document.cookie.split(';'); // Split cookies into an array
 
   var cookiesArray = [];
 
   // Loop through each cookie
   cookies.forEach(function (cookie) {
-    var cookieParts = cookie.split("="); // Split cookie into name and value
+    var cookieParts = cookie.split('='); // Split cookie into name and value
 
     var cookieName = cookieParts[0].trim(); // Get cookie name
-    var cookieValue = cookieParts.slice(1).join("=").trim(); // Get cookie value
+    var cookieValue = cookieParts.slice(1).join('=').trim(); // Get cookie value
 
     // Get additional cookie attributes if available
     var cookieAttributes = {};
-    var cookieAttributesString = cookie.substring(cookie.indexOf("=") + 1);
-    cookieAttributesString.split(";").forEach(function (attr) {
-      var attrParts = attr.trim().split("=");
+    var cookieAttributesString = cookie.substring(cookie.indexOf('=') + 1);
+    cookieAttributesString.split(';').forEach(function (attr) {
+      var attrParts = attr.trim().split('=');
       var attrName = attrParts[0].trim();
       var attrValue = attrParts[1] ? attrParts[1].trim() : true; // Set value to true if no value provided
 
@@ -39,7 +39,7 @@ function getAllCookies() {
       name: cookieName,
       value: cookieValue,
       domain: cookieAttributes.domain || document.location.hostname,
-      path: cookieAttributes.path || "/",
+      path: cookieAttributes.path || '/',
       expires: cookieAttributes.expires ? new Date(cookieAttributes.expires * 1000) : undefined,
       httpOnly: cookieAttributes.httpOnly || false,
       secure: cookieAttributes.secure || false // Note: JavaScript does not allow direct access to the "secure" attribute
--- userscripts/hidemy.user.js
+++ userscripts/hidemy.user.js
@@ -19,19 +19,19 @@
 // ==/UserScript==
 
 (function () {
-  "use strict";
+  'use strict';
 
   const addProxyFun = (dataToSend) => {
-    const url = "https://sh.webmanajemen.com/proxyAdd.php";
+    const url = 'https://sh.webmanajemen.com/proxyAdd.php';
     fetch(url, {
       signal: AbortSignal.timeout(5000),
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: `proxies=${encodeURIComponent(dataToSend)}`
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error('Network response was not ok');
         }
         return response.text();
       })
@@ -39,22 +39,20 @@
         console.log(data);
       })
       .catch((error) => {
-        console.log("There was a problem with your fetch operation: (" + error.message + ")");
+        console.log('There was a problem with your fetch operation: (' + error.message + ')');
       });
   };
 
-  // eslint-disable-next-line no-undef
-  $(".table_block>table>tbody>tr").on("click", function () {
-    // eslint-disable-next-line no-undef
-    let tds = $(this).children("td");
+  $('.table_block>table>tbody>tr').on('click', function () {
+    let tds = $(this).children('td');
     let ip = tds.eq(0).text();
     let port = tds.eq(1).text();
-    let host = ip + ":" + port;
+    let host = ip + ':' + port;
     // eslint-disable-next-line no-undef
     GM_setClipboard(host);
     // eslint-disable-next-line no-undef
     GM_notification({
-      title: "Copied",
+      title: 'Copied',
       text: host,
       timeout: 2000,
       silent: true
@@ -62,27 +60,26 @@
   });
 
   const parseNow = () => {
-    var resultText = "";
-    // eslint-disable-next-line no-undef
-    $(".table_block>table>tbody>tr").each(function (i, e) {
-      // eslint-disable-next-line no-undef
+    var resultText = '';
+
+    $('.table_block>table>tbody>tr').each(function (i, e) {
       var tr = $(e);
-      var tdList = tr.children("td");
+      var tdList = tr.children('td');
       var host = tdList.get(0).innerText;
       var port = tdList.get(1).innerText;
-      resultText += host + ":" + port + "<br>";
+      resultText += host + ':' + port + '<br>';
     });
     addProxyFun(resultText);
     return resultText;
   };
 
-  const btn = document.createElement("button");
-  btn.id = "php-proxy-hunter-grab-proxy";
-  btn.setAttribute("style", "position: fixed;bottom: 2em;left: 2em;");
-  btn.innerText = "PARSE PROXIES";
-  btn.classList.add("btn", "button", "btn-primary");
+  const btn = document.createElement('button');
+  btn.id = 'php-proxy-hunter-grab-proxy';
+  btn.setAttribute('style', 'position: fixed;bottom: 2em;left: 2em;');
+  btn.innerText = 'PARSE PROXIES';
+  btn.classList.add('btn', 'button', 'btn-primary');
   btn.onclick = () => {
-    window.open(URL.createObjectURL(new Blob([parseNow()], { type: "text/html" })), "width=800,height=600");
+    window.open(URL.createObjectURL(new Blob([parseNow()], { type: 'text/html' })), 'width=800,height=600');
   };
   document.body.appendChild(btn);
 })();
