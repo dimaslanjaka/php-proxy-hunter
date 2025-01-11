d41cf0397848b50faccae0e6c991a573a50c6af9
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-11 09:01:13 +0700

    feat: add build scripts for project automation in Bash and Batch

---
+++ bin/build-project.cmd
@@ -0,0 +1,13 @@
+@echo off
+REM Get the directory of the current Batch script
+SET SCRIPT_DIR=%~dp0
+SET SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+ECHO Current script directory: %SCRIPT_DIR%
+
+REM Set CWD to the parent directory of the script's directory
+FOR %%I IN ("%SCRIPT_DIR%") DO SET CWD=%%~dpI
+SET CWD=%CWD:~0,-1%
+ECHO Current working directory (parent of script directory): %CWD%
+
+REM Run the Node.js script
+node "%SCRIPT_DIR%\build-project.cjs"
