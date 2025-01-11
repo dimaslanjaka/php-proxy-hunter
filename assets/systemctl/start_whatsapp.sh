414249d379f4b2cef1840988b45a6cf691b98547
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-11 15:59:50 +0700

    fix: improve NVM directory detection

--- assets/systemctl/start_whatsapp.sh
+++ assets/systemctl/start_whatsapp.sh
@@ -19,17 +19,21 @@ else
   echo "No .env file found in $CWD"
 fi
 
-export NVM_DIR="$HOME/.nvm"
+if [ -d "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+elif [ -d "/usr/local/nvm" ]; then
+  export NVM_DIR="/usr/local/nvm"
+else
+  echo "Neither $HOME/.nvm nor /usr/local/nvm exists."
+  exit 1
+fi
 
 # Check if NVM is installed and load it
 if [ -d "$NVM_DIR" ] && [ -s "$NVM_DIR/nvm.sh" ]; then
   # Load NVM into the shell session
   . "$NVM_DIR/nvm.sh"
-
-  # Enable corepack and install yarn
-  corepack enable yarn && yarn install
 else
-  echo "NVM is not installed. Please install NVM first."
+  echo "NVM is not installed. Please install NVM first. (readme-nodejs.md)"
   exit 1
 fi
 
@@ -37,5 +41,4 @@ fi
 # node /var/www/html/dist/whatsapp-xl.js
 # "$CWD/node_modules/.bin/nodemon" --exec "/root/.nvm/versions/node/v20.18.1/bin/node" /var/www/html/whatsapp-xl.js
 # nodemon --watch dist/whatsapp-xl.js dist/whatsapp-xl.js
-"$CWD/node_modules/.bin/nodemon" --watch "$CWD/dist/whatsapp-xl.js" "$CWD/dist/whatsapp-xl.js"
-
+"$CWD/node_modules/.bin/nodemon" --watch "$CWD/dist/whatsapp.js" --watch "$CWD/dist/whatsapp_handlers/**/*.js" "$CWD/dist/whatsapp.js"
