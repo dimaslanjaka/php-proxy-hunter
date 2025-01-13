0842583e5e100a140a0677a1db60af6fd329dc97
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-13 11:58:09 +0700

    feat: add nodemon configurations

---
+++ nodemon.restart.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# run with
+# nodemon --exec [your command]
+# eg:
+# nodemon --exec build-project
+
+LOCKFILE="tmp/runners/nodemon.lock"
+
+# Create the directory for the lock file if it doesn't exist
+mkdir -p "$(dirname "$LOCKFILE")"
+
+# Function to remove the lock file on exit
+cleanup() {
+    rm -f "$LOCKFILE"
+}
+
+# Check if the lock file exists
+if [ -f "$LOCKFILE" ]; then
+    echo "Script is already running."
+else
+    touch "$LOCKFILE"
+    trap cleanup EXIT
+    clear
+    gulp prepare
+    if [ -f "$LOCKFILE" ]; then
+        rm -f "$LOCKFILE"
+    fi
+    # node --harmony -r ts-node/register node_browser/express-server.js
+fi
