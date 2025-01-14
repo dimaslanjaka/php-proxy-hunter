ddfcba98025cf039efca6c582f61412b2f30b9ea
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-14 11:34:24 +0700

    fix: use nodemon for automatic restarts

--- assets/systemctl/start_whatsapp.sh
+++ assets/systemctl/start_whatsapp.sh
@@ -43,6 +43,7 @@ NODE_PATH=$(realpath $(which node))
 # node /var/www/html/dist/whatsapp-xl.js
 # "$CWD/node_modules/.bin/nodemon" --exec "/root/.nvm/versions/node/v20.18.1/bin/node" /var/www/html/whatsapp-xl.js
 # nodemon --watch dist/whatsapp-xl.js dist/whatsapp-xl.js
-# "$CWD/node_modules/.bin/nodemon" --watch "$CWD/dist/whatsapp.js" --watch "$CWD/dist/whatsapp_handlers/**/*.js" "$CWD/dist/whatsapp.js"
 
-"$NODE_PATH" "$CWD/dist/whatsapp.js"
+"$CWD/node_modules/.bin/nodemon" --watch "$CWD/dist/whatsapp.js" --watch "$CWD/dist/whatsapp_handlers/**/*.js" "$CWD/dist/whatsapp.js"
+
+# "$NODE_PATH" "$CWD/dist/whatsapp.js"
