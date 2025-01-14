1e9256c4dfab1dbc9310d27fdce0b24938696fd2
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-14 11:34:46 +0700

    feat: add phone number extraction and WhatsApp logging functionality

--- node_backend/test/extractPhoneNumber.ts
+++ node_backend/test/extractPhoneNumber.ts
@@ -6,3 +6,7 @@ console.log(extractPhoneNumber('/login 087857297039')); // 6287857297039
 console.log(extractPhoneNumber('/login +6287857297039')); // 6287857297039
 console.log(extractPhoneNumber('/login +62 878-5729-7039')); // 6287857297039
 console.log(extractPhoneNumber('/login 0878-5729-7039')); // 6287857297039
+console.log(extractPhoneNumber('My number is +62 8785-4312-284')); // 6287854312284
+console.log(extractPhoneNumber('Call me at 087854312284')); // 6287854312284
+console.log(extractPhoneNumber('Phone: 6287854312284')); // 6287854312284
+console.log(extractPhoneNumber('This text has no phone number')); // ''
