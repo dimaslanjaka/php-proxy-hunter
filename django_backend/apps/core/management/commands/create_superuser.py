991047de8cf1c03163ad235557d544133e4ccc07
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-20 02:15:34 +0700

    fix: enhance superuser command help text and improve code formatting

--- django_backend/apps/core/management/commands/create_superuser.py
+++ django_backend/apps/core/management/commands/create_superuser.py
@@ -1,22 +1,20 @@
-# python manage.py create_superuser
-
 import environ
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Create or update a superuser with credentials from the .env file'
+    help = "Create or update a superuser with credentials from the .env file\n\npython manage.py create_superuser"
 
     def handle(self, *args, **kwargs):
         # Initialize environment variables
         env = environ.Env()
         environ.Env.read_env()
 
-        # Retrieve environment variables
-        username = env('DJANGO_SUPERUSER_USERNAME')
-        password = env('DJANGO_SUPERUSER_PASSWORD')
-        email = env('DJANGO_SUPERUSER_EMAIL')
+        # Retrieve environment variables (/.env)
+        username = env("DJANGO_SUPERUSER_USERNAME")
+        password = env("DJANGO_SUPERUSER_PASSWORD")
+        email = env("DJANGO_SUPERUSER_EMAIL")
 
         UserModel = get_user_model()
 
@@ -26,7 +24,13 @@ class Command(BaseCommand):
             user.email = email
             user.set_password(password)
             user.save()
-            self.stdout.write(self.style.SUCCESS(f'Successfully updated superuser "{username}"'))
+            self.stdout.write(
+                self.style.SUCCESS(f'Successfully updated superuser "{username}"')
+            )
         else:
-            UserModel.objects.create_superuser(username=username, password=password, email=email)
-            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser "{username}"'))
+            UserModel.objects.create_superuser(
+                username=username, password=password, email=email
+            )
+            self.stdout.write(
+                self.style.SUCCESS(f'Successfully created superuser "{username}"')
+            )
