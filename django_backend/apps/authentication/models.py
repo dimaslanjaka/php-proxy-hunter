b92d3a671e5d69fbbae9e402b009897b0033c82e
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-20 06:07:23 +0700

    fix: update UserBalance model to use Decimal for saldo default value

--- django_backend/apps/authentication/models.py
+++ django_backend/apps/authentication/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from decimal import Decimal
 
 
 class UserBalance(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    saldo = models.DecimalField(
+        max_digits=10, decimal_places=2, default=Decimal("0.00")
+    )
 
     def __str__(self):
         # title admin page /admin/authentication/userbalance/<user_id>/change/
