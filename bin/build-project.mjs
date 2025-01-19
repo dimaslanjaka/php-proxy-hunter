f9a45a4dc3c6f1457ddfc315b7034b8c30b537c0
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-19 07:34:39 +0700

    fix: handle potential undefined WHATSAPP_ADMIN in environment config

--- bin/build-project.mjs
+++ bin/build-project.mjs
@@ -18,7 +18,7 @@ const envConfig = {
   GITHUB_TOKEN_READ_ONLY: dotenvConfig.GITHUB_TOKEN_READ_ONLY, // Add GitHub token
   isDebug: dotenvConfig.isDebug, // Add debug flag
   PROJECT_DIR: cwd, // Add project directory path
-  WHATSAPP_ADMIN: dotenvConfig.WHATSAPP_ADMIN.split(',') // Add whatsapp admin list
+  WHATSAPP_ADMIN: dotenvConfig.WHATSAPP_ADMIN?.split(',') || [] // Add whatsapp admin list
 };
 
 // Write environment config to a JSON file
