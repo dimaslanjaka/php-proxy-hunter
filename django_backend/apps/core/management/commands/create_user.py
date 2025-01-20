d9fe66b51c45ab11fa45aa64eff098ed9bcaae93
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-20 07:21:24 +0700

    feat: enhance create_user command with detailed output and improved user creation logic

--- django_backend/apps/core/management/commands/create_user.py
+++ django_backend/apps/core/management/commands/create_user.py
@@ -3,34 +3,44 @@ from django.core.management.base import BaseCommand, CommandError
 
 User = get_user_model()
 
-# python manage.py create_user demo@gmail.com demo pass
-
 
 class Command(BaseCommand):
-    help = 'Creates a new user'
+    help = "Creates a new user\npython manage.py create_user demo@gmail.com demo pass"
 
     def add_arguments(self, parser):
-        parser.add_argument('email', type=str, help='Email address of the user')
-        parser.add_argument('username', type=str, help='Username of the user')
-        parser.add_argument('password', type=str, help='Password of the user')
+        parser.add_argument("email", type=str, help="Email address of the user")
+        parser.add_argument("username", type=str, help="Username of the user")
+        parser.add_argument("password", type=str, help="Password of the user")
 
     def handle(self, *args, **options):
-        email = options.get('email') or 'default@example.com'  # Default email if not provided
-        username = options['username']
-        password = options['password']
-        print(f"create user {username} {password} {email}")
+        email = (
+            options.get("email") or "default@example.com"
+        )  # Default email if not provided
+        username = options["username"]
+        password = options["password"]
+        print(
+            f"Creating user with username: {username}, password: {password}, email: {email}"
+        )
 
         try:
             if User.objects.filter(username=username).exists():
+                # Create user
                 user = User.objects.get(username=username)
                 user.email = email
                 user.set_password(password)
                 user.save()
-                self.stdout.write(self.style.SUCCESS(f'Successfully updated user "{username}"'))
+                self.stdout.write(
+                    self.style.SUCCESS(f'Successfully updated user "{username}"')
+                )
             else:
-                user = User.objects.create_user(email=email, username=username, password=password)
+                # Change user password
+                user = User.objects.create_user(
+                    email=email, username=username, password=password
+                )
                 user.set_password(password)
                 user.save()
-                self.stdout.write(self.style.SUCCESS(f'Successfully created user: {username}'))
+                self.stdout.write(
+                    self.style.SUCCESS(f"Successfully created user: {username}")
+                )
         except Exception as e:
-            raise CommandError(f'Failed to create user: {username}. Error: {str(e)}')
+            raise CommandError(f"Failed to create user: {username}. Error: {str(e)}")
