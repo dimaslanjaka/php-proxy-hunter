277a9d2fb1497ad0da6c673f41b4dc3806229515
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-23 09:17:25 +0700

    feat: compile php frontend javascript

--- rollup.php.js
+++ rollup.php.js
@@ -3,9 +3,11 @@ import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import fs from 'fs';
+import * as glob from 'glob';
 import jsonc from 'jsonc-parser';
-import path from 'path';
+import path from 'upath';
 import { fileURLToPath } from 'url';
+import { isDebug } from './src/func.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,7 +23,7 @@ export const external = Object.keys(pkg.dependencies)
 /**
  * @type {import('rollup').RollupOptions}
  */
-export const proxyManager = {
+const proxyManager = {
   input: './proxyManager-src.js',
   output: {
     file: './proxyManager.js',
@@ -30,9 +32,39 @@ export const proxyManager = {
   plugins: [
     resolve({ preferBuiltins: true }), // Resolve node_modules packages
     json(), // Support for JSON files
-    commonjs(),
-    terser({ sourceMap: false }) // Minify the output
+    commonjs()
   ]
 };
+if (!isDebug()) {
+  // Minify the output on production
+  proxyManager.plugins.push(terser({ sourceMap: false }));
+}
+export { proxyManager };
 
-export default proxyManager;
+const findJs = glob.globSync('views/assets/**/*.js', { cwd: __dirname }).map((f) => {
+  return path.toUnix(f);
+});
+const phpJs = findJs.map((input) => {
+  /**
+   * @type {import('rollup').RollupOptions}
+   */
+  const config = {
+    input,
+    output: {
+      file: `public/php/${input}`,
+      format: 'iife'
+    },
+    plugins: [
+      resolve({ preferBuiltins: true }), // Resolve node_modules packages
+      json(), // Support for JSON files
+      commonjs()
+    ]
+  };
+  if (!isDebug()) {
+    // Minify the output on production
+    config.plugins.push(terser({ sourceMap: false }));
+  }
+  return config;
+});
+
+export default [proxyManager, ...phpJs];
