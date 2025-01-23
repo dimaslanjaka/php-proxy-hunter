2c4bd511a8ece25643c9089d64030709e2d15c94
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-23 15:30:50 +0700

    chore: separate profile card

--- views/assets/js/dashboard.js
+++ views/assets/js/dashboard.js
@@ -1,12 +1,3 @@
-import $ from 'jquery';
-import { getUserData } from './template.js';
+import initProfileCard from './profile-card.js';
 
-getUserData()
-  .then((data) => {
-    $('#user-saldo').html(data.saldo);
-    $('#user-email').html(data.email);
-    let fullname = `${data.first_name} ${data.last_name}`.trim();
-    if (fullname.length === 0) fullname = data.username;
-    $('#user-fullname').html(fullname);
-  })
-  .catch(console.error);
+initProfileCard();
