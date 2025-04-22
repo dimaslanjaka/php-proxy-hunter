166d604d66f3255986dacf04a8064fb908ba332e
dimaslanjaka <dimaslanjaka@gmail.com>
2025-04-22 08:09:19 +0700

    refactor: add ProxyDetails interface

---
+++ types/proxy.d.ts
@@ -0,0 +1,24 @@
+export interface ProxyDetails {
+  [key: string]: any;
+  proxy: string;
+  latency: string;
+  last_check: string;
+  type: string;
+  region: string;
+  city: string;
+  country: string;
+  timezone: string;
+  latitude: string;
+  longitude: string;
+  anonymity: string;
+  https: string;
+  status: string;
+  private: string;
+  lang: string;
+  useragent: string;
+  webgl_vendor: string;
+  webgl_renderer: string;
+  browser_vendor: string;
+  username: string;
+  password: string;
+}
