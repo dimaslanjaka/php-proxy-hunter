8500612d434c46f3cb0b73a418fd3e86e7c7394d
dimaslanjaka <dimaslanjaka@gmail.com>
2025-06-18 20:58:22 +0700

    fix: prefix log messages with context identifiers for better traceability

--- controllers/ListDuplicateProxyController.php
+++ controllers/ListDuplicateProxyController.php
@@ -190,5 +190,5 @@ if (php_sapi_name() === 'cli' && realpath(__FILE__) === realpath($_SERVER['argv'
     return "$key: " . count($value) . " proxies";
   }, array_keys($data), $data);
 
-  $controller->log($result);
+  $controller->log("[LIST-DUPLICATE] " . json_encode($result));
 }
