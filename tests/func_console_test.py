f7c5983cc1581291d2521aa65a6308a2be25dcf6
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-08 03:12:04 +0700

    test: add checks for empty file reads in func_console_test.py

--- tests/func_console_test.py
+++ tests/func_console_test.py
@@ -19,16 +19,22 @@ if __name__ == "__main__":
     truncate_file_content(clean_file)
     log_file(clean_file, print_args=False, *messages)
     read = read_file(clean_file)
+    if not read:
+        raise ValueError("clean_file is empty, check the log_file function")
     print("clean_file should not have ANSI ", contains_ansi_codes(read) == False)
 
     ansi_file = get_relative_path("tmp/data/test-with-ansi.txt")
     truncate_file_content(ansi_file)
     log_file(ansi_file, print_args=False, remove_ansi=False, *messages)
     read = read_file(ansi_file)
+    if not read:
+        raise ValueError("ansi_file is empty, check the log_file function")
     print("ansi_file should contains ANSI ", contains_ansi_codes(read) == True)
 
     html_file = get_relative_path("tmp/data/test-with-ansi.html")
     truncate_file_content(html_file)
     log_file(html_file, print_args=False, ansi_html=True, *messages)
     read = read_file(html_file)
+    if not read:
+        raise ValueError("html_file is empty, check the log_file function")
     print("html_file should not have ANSI ", contains_ansi_codes(read) == False)
