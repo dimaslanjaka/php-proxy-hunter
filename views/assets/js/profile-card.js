b0941b5c661bc71b2f2f79d9703d829ae460cb66
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-21 05:34:37 +0700

    fix(profile-card): ensure saldo is displayed correctly with template literals

--- views/assets/js/profile-card.js
+++ views/assets/js/profile-card.js
@@ -4,7 +4,7 @@ import { getUserData } from './template.js';
 export default async function initProfileCard() {
   try {
     const data = await getUserData();
-    $('#user-saldo').html(data.saldo);
+    $('#user-saldo').html(`${data.saldo}`);
     $('#user-email').html(data.email);
     let fullname = `${data.first_name} ${data.last_name}`.trim();
     if (fullname.length === 0) fullname = data.username;
