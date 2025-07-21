0a3ff1b563d9aa4fd33d4380e93dd8d860535c55
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-21 13:41:11 +0700

    feat(requirements): add nuitka to Linux package requirements

--- requirements_install.py
+++ requirements_install.py
@@ -83,6 +83,7 @@ def generate_requirements() -> bool:
         "Linux": [
             "uwsgi @ https://github.com/unbit/uwsgi/archive/f931938.zip",
             "gunicorn",
+            "nuitka",
             "numpy",
         ],
     }
