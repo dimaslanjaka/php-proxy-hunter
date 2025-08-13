688720f0f803c9d469d98355a0eb07d843284809
Dimas Lanjaka <dimaslanjaka@gmail.com>
2025-08-13 20:31:02 +0700

    feat(vite-gh-pages): add .no_jekyll file creation to prevent Jekyll processing

--- vite-gh-pages.js
+++ vite-gh-pages.js
@@ -1,5 +1,7 @@
 import { build, mergeConfig } from 'vite';
 import config from './vite.config.js';
+import fs from 'fs-extra';
+import path from 'upath';
 
 async function buildForGithubPages() {
   const mergedConfig = mergeConfig(config, { base: '/php-proxy-hunter/' });
@@ -8,6 +10,12 @@ async function buildForGithubPages() {
       console.log('Build successful for GitHub Pages');
     })
     .catch(console.error);
+  // add .no_jekyll file to prevent Jekyll processing
+  const noJekyllPath = path.join(mergedConfig.build.outDir, '.no_jekyll');
+  if (!fs.existsSync(noJekyllPath)) {
+    fs.writeFileSync(noJekyllPath, '');
+    console.log('.no_jekyll file created to prevent Jekyll processing');
+  }
 }
 
 buildForGithubPages();
