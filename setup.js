327f6af7cc9397e1345c7be4cebf34bf5a7fffe8
Dimas Lanjaka <dimaslanjaka@gmail.com>
2025-08-18 05:57:20 +0700

    feat(setup): update setup.js to run browserslist-db update
    - Import spawnSync from 'child_process'
    - Add command to run 'npx --yes update-browserslist-db@latest' after build steps

--- setup.js
+++ setup.js
@@ -1,6 +1,8 @@
 import './.env.mjs';
 import { buildTailwind } from './tailwind.build.js';
 import { copyIndexHtml } from './vite-plugin.js';
+import { spawnSync } from 'child_process';
 
 buildTailwind();
 copyIndexHtml();
+spawnSync('npx', ['--yes', 'update-browserslist-db@latest'], { stdio: 'inherit', shell: true });
