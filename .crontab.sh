246c584541bd6d0640f159a472b982204f74f29e
Dimas Lanjaka <dimaslanjaka@gmail.com>
2025-08-18 05:45:05 +0700

    fix(crontab): improve indentation and add MySQL backup functionality

--- .crontab.sh
+++ .crontab.sh
@@ -11,63 +11,63 @@ USER="www-data"
 
 # Detect python virtual bin by operating system
 if [ "$(uname -s)" = "Darwin" ] || [ "$(uname -s)" = "Linux" ]; then
-  # Unix-based systems (Linux, macOS)
-  VENV_BIN="$CWD/venv/bin"
+    # Unix-based systems (Linux, macOS)
+    VENV_BIN="$CWD/venv/bin"
 else
-  # Assume Windows
-  VENV_BIN="$CWD/venv/Scripts"
+    # Assume Windows
+    VENV_BIN="$CWD/venv/Scripts"
 fi
 
 # Check if PATH is set
 if [ -z "$PATH" ]; then
-  export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$CWD/bin:$CWD/node_modules/.bin:$CWD/vendor/bin:$VENV_BIN
+    export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$CWD/bin:$CWD/node_modules/.bin:$CWD/vendor/bin:$VENV_BIN
 else
-  export PATH=$PATH:$CWD/bin:$CWD/node_modules/.bin:$CWD/vendor/bin:$VENV_BIN
+    export PATH=$PATH:$CWD/bin:$CWD/node_modules/.bin:$CWD/vendor/bin:$VENV_BIN
 fi
 
 # Load .env file
 if [ -f "$CWD/.env" ]; then
-  source "$CWD/.env"
+    source "$CWD/.env"
 fi
 
 r_cmd() {
-  COMMAND_RUNNER=$1
-  FILE_PATH=$2
-  COMMAND_ARGS=${@:3}
-  is_running=false
+    COMMAND_RUNNER=$1
+    FILE_PATH=$2
+    COMMAND_ARGS=${@:3}
+    is_running=false
 
-  # check script is running
-  if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
-    # Use ps for Windows (Cygwin)
-    if ps aux | grep -v grep | grep "$COMMAND_RUNNER $CWD/$FILE_PATH" >/dev/null; then
-      is_running=true
-    fi
-    wmic process where "name='$COMMAND_RUNNER.exe'" get commandline /format:list | grep -i "$FILE_PATH"
-    if wmic process where "name='$COMMAND_RUNNER.exe'" get commandline /format:list | grep -i "$FILE_PATH" >/dev/null; then
-      is_running=true
-    fi
-  else
-    # Use pgrep for Unix-like systems
-    if pgrep -f "$COMMAND_RUNNER $CWD/$FILE_PATH" >/dev/null; then
-      is_running=true
-    fi
-  fi
-
-  if $is_running; then
-    echo "$CWD/$FILE_PATH is still running."
-  else
-    # Check if OS is Linux and nginx is installed
-    if [[ "$(uname)" == "Linux" && -x "$(command -v nginx)" ]]; then
-      USER="www-data"
-      COMMAND="source $CWD/venv/bin/activate && $COMMAND_RUNNER $FILE_PATH $COMMAND_ARGS"
-      echo "Executing as $USER: $COMMAND"
-      sudo -u "$USER" -H bash -c "$COMMAND"
+    # check script is running
+    if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+        # Use ps for Windows (Cygwin)
+        if ps aux | grep -v grep | grep "$COMMAND_RUNNER $CWD/$FILE_PATH" >/dev/null; then
+            is_running=true
+        fi
+        wmic process where "name='$COMMAND_RUNNER.exe'" get commandline /format:list | grep -i "$FILE_PATH"
+        if wmic process where "name='$COMMAND_RUNNER.exe'" get commandline /format:list | grep -i "$FILE_PATH" >/dev/null; then
+            is_running=true
+        fi
     else
-      COMMAND="source $CWD/venv/Scripts/activate && $COMMAND_RUNNER $FILE_PATH $COMMAND_ARGS"
-      echo "Executing: $COMMAND"
-      eval "$COMMAND"
+        # Use pgrep for Unix-like systems
+        if pgrep -f "$COMMAND_RUNNER $CWD/$FILE_PATH" >/dev/null; then
+            is_running=true
+        fi
     fi
-  fi
+
+    if $is_running; then
+        echo "$CWD/$FILE_PATH is still running."
+    else
+        # Check if OS is Linux and nginx is installed
+        if [[ "$(uname)" == "Linux" && -x "$(command -v nginx)" ]]; then
+            USER="www-data"
+            COMMAND="source $CWD/venv/bin/activate && $COMMAND_RUNNER $FILE_PATH $COMMAND_ARGS"
+            echo "Executing as $USER: $COMMAND"
+            sudo -u "$USER" -H bash -c "$COMMAND"
+        else
+            COMMAND="source $CWD/venv/Scripts/activate && $COMMAND_RUNNER $FILE_PATH $COMMAND_ARGS"
+            echo "Executing: $COMMAND"
+            eval "$COMMAND"
+        fi
+    fi
 }
 
 # Example usage:
@@ -79,66 +79,80 @@ r_cmd() {
 
 # Helper function to check if it's time to run a job
 should_run_job() {
-  local file_path=$1
-  local interval_hours=$2
+    local file_path=$1
+    local interval_hours=$2
 
-  current_time=$(date +%s)                       # Current timestamp in seconds
-  interval_seconds=$((interval_hours * 60 * 60)) # Convert hours to seconds
+    current_time=$(date +%s)                       # Current timestamp in seconds
+    interval_seconds=$((interval_hours * 60 * 60)) # Convert hours to seconds
 
-  # Check if timestamp file exists
-  if [ -f "$file_path" ]; then
-    last_fetch=$(cat "$file_path")
-    elapsed_time=$((current_time - last_fetch))
+    # Check if timestamp file exists
+    if [ -f "$file_path" ]; then
+        last_fetch=$(cat "$file_path")
+        elapsed_time=$((current_time - last_fetch))
 
-    if [ $elapsed_time -ge $interval_seconds ]; then
-      # Update the timestamp file with the current time
-      echo "$current_time" >"$file_path"
-      return 0 # True, it's time to run the job
+        if [ $elapsed_time -ge $interval_seconds ]; then
+            # Update the timestamp file with the current time
+            echo "$current_time" >"$file_path"
+            return 0 # True, it's time to run the job
+        else
+            return 1 # False, it's not time to run the job
+        fi
     else
-      return 1 # False, it's not time to run the job
+        # Create the file and update it with the current time
+        echo "$current_time" >"$file_path"
+        return 0 # True, file not found, so it's time to run the job
     fi
-  else
-    # Create the file and update it with the current time
-    echo "$current_time" >"$file_path"
-    return 0 # True, file not found, so it's time to run the job
-  fi
 }
 
 mkdir -p tmp/crontab
 
 # run every 6 hours
 if should_run_job "tmp/crontab/6-h" 6; then
-  djm sync_proxies
+    djm sync_proxies
 fi
 
 # run every hour
 if should_run_job "tmp/crontab/1-h" 1; then
-  php "$CWD/send_curl.php" --url=https://sh.webmanajemen.com:8443/proxy/check
-  djm check_proxies --max=100
-  djm filter_dups --max=100
+    php "$CWD/send_curl.php" --url=https://sh.webmanajemen.com:8443/proxy/check
+    djm check_proxies --max=100
+    djm filter_dups --max=100
 fi
 
 # run every 3 hours
 if should_run_job "tmp/crontab/3-h" 3; then
-  bash "$CWD/bin/check-proxy-parallel"
+    bash "$CWD/bin/check-proxy-parallel"
 fi
 
 # run every 4 hours
 if should_run_job "tmp/crontab/4-h" 4; then
-  python "$CWD/proxyFetcher.py"
+    python "$CWD/proxyFetcher.py"
 fi
 
 # run every 12 hours
 if should_run_job "tmp/crontab/12-h" 12; then
-  if [ -f "tmp/database.sqlite-wal" ]; then
-    echo "Checkpointing and truncating WAL file..."
-    sqlite3 tmp/database.sqlite "PRAGMA wal_checkpoint(TRUNCATE);"
-    echo "tmp/database.sqlite WAL file truncated."
-  fi
+    # Truncate WAL files for SQLite databases
+    if [ -f "$CWD/tmp/database.sqlite-wal" ]; then
+        echo "Checkpointing and truncating WAL file..."
+        sqlite3 "$CWD/tmp/database.sqlite" "PRAGMA wal_checkpoint(TRUNCATE);"
+        echo "$CWD/tmp/database.sqlite WAL file truncated."
+    fi
+    if [ -f "$CWD/src/database.sqlite-wal" ]; then
+        echo "Checkpointing and truncating WAL file..."
+        sqlite3 "$CWD/src/database.sqlite" "PRAGMA wal_checkpoint(TRUNCATE);"
+        echo "$CWD/src/database.sqlite WAL file truncated."
+    fi
+fi
 
-  if [ -f "src/database.sqlite-wal" ]; then
-    echo "Checkpointing and truncating WAL file..."
-    sqlite3 src/database.sqlite "PRAGMA wal_checkpoint(TRUNCATE);"
-    echo "src/database.sqlite WAL file truncated."
-  fi
+# run every 24 hours
+if should_run_job "tmp/crontab/24-h" 24; then
+    BACKUP_DIR="$CWD/backups"
+    # backup database mysql
+    if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_PASSWORD" ] && [ -n "$MYSQL_DATABASE" ]; then
+        mkdir -p "$BACKUP_DIR"
+        BACKUP_FILE="$BACKUP_DIR/mysql-backup-$(date +%Y%m%d-%H%M%S).sql"
+        mysqldump -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" > "$BACKUP_FILE"
+        echo "MySQL backup saved to $BACKUP_FILE"
+    else
+        echo "MySQL credentials not set in .env"
+    fi
 fi
