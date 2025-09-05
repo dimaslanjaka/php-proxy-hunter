cde945b0e7c6822dc90066e3064468b423ad2a6b
dimaslanjaka <dimaslanjaka@gmail.com>
2025-09-05 16:29:39 +0700

    refactor: update PHP CS Fixer and PHPCS configuration to align with PSR-12 standards

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,21 +7,28 @@ use PhpCsFixer\Finder;
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 $finder = Finder::create()
-    ->in(__DIR__)
-    ->exclude(['vendor', 'node_modules']);
+  ->in(__DIR__)
+  ->exclude(['vendor', 'node_modules'])
+  ->name('*.php')
+  ->ignoreDotFiles(true)
+  ->ignoreVCS(true);
 
 $config = new Config();
 $config->setParallelConfig(ParallelConfigFactory::detect());
 $config->setIndent(str_pad('', 2));
 
 return $config->setRules([
-    '@PSR2' => true,
-    'array_syntax' => ['syntax' => 'short'],
-    'no_unused_imports' => true,
-    'braces' => [
-        'allow_single_line_closure' => true,
-        'position_after_functions_and_oop_constructs' => 'same',
-    ],
+  '@PSR12'                      => true,
+  'array_syntax'                => ['syntax' => 'short'],
+  'no_unused_imports'           => true,
+  'single_quote'                => true,
+  'binary_operator_spaces'      => ['default' => 'align_single_space_minimal'],
+  'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+  'braces'                      => [
+    'allow_single_line_closure'                   => true,
+    'position_after_functions_and_oop_constructs' => 'same',
+  ],
 ])
-    ->setFinder($finder)
-    ->setCacheFile('tmp/.php-cs-fixer.cache');
+  ->setFinder($finder)
+  ->setCacheFile('tmp/.php-cs-fixer.cache')
+  ->setRiskyAllowed(true);
