ff5c2c167a6f0c46903fc96892a4dd1279e6323a
dimaslanjaka <dimaslanjaka@gmail.com>
2025-09-15 09:25:38 +0700

    feat(session): add FileSessionHandler for filesystem-based session management
    - implemented FileSessionHandler class using SessionHandlerInterface
    - supports configurable session file path, prefix, and postfix
    - handles reading, writing, destroying, and garbage collection of session files
    - validates directory existence and writability on initialization

---
+++ src/FileSessionHandler.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace PhpProxyHunter;
+
+use Exception;
+use SessionHandlerInterface;
+
+class FileSessionHandler implements SessionHandlerInterface
+{
+  protected $sess_path;
+  protected $prefix;
+  protected $postfix;
+
+  /**
+   * @throws Exception
+   */
+  public function __construct($sess_path, $prefix = 'sess_', $postfix = '')
+  {
+    if (!is_dir($sess_path)) {
+      throw new Exception("Cannot use FileSessionHandler, directory '{$sess_path}' not found", 1);
+    }
+
+    if (!is_writable($sess_path)) {
+      throw new Exception("Cannot use FileSessionHandler, directory '{$sess_path}' is not writable", 2);
+    }
+
+    $this->sess_path = $sess_path;
+    $this->prefix    = $prefix;
+    $this->postfix   = $postfix;
+  }
+
+  public function open($save_path, $sess_name): bool
+  {
+    return true;
+  }
+
+  public function close(): bool
+  {
+    return true;
+  }
+
+  public function read($sess_id): string
+  {
+    return (string)@file_get_contents("{$this->sess_path}/{$this->prefix}{$sess_id}{$this->postfix}");
+  }
+
+  public function write($sess_id, $data): bool
+  {
+    return !(false === file_put_contents("{$this->sess_path}/{$this->prefix}{$sess_id}{$this->postfix}", $data));
+  }
+
+  public function destroy($sess_id): bool
+  {
+    $file = "{$this->sess_path}/{$this->prefix}{$sess_id}{$this->postfix}";
+
+    if (file_exists($file)) {
+      unlink($file);
+    }
+
+    return true;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function gc($max_lifetime): int
+  {
+    $deleted = 0;
+    foreach (glob("{$this->sess_path}/{$this->prefix}*") as $file) {
+      if (file_exists($file)) {
+        if (filemtime($file) + $max_lifetime < time()) {
+          unlink($file);
+          $deleted++;
+        }
+      }
+    }
+    return $deleted;
+  }
+}
