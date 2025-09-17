ac42fbc88974c218c51854bc5f719a6f1217999d
dimaslanjaka <dimaslanjaka@gmail.com>
2025-09-17 15:45:57 +0700

    fix(shim): update json_validate function to match PHP 8.3 polyfill specifications

--- src/PhpProxyHunter/shim.php
+++ src/PhpProxyHunter/shim.php
@@ -1,15 +1,18 @@
 <?php
 
+
 if (!function_exists('json_validate')) {
   /**
-   * Validates if a string is valid JSON.
+   * Polyfill for PHP 8.3's json_validate().
    *
-   * @param string $string The string to validate.
+   * @param string $json  The JSON string to validate.
+   * @param int    $depth The maximum depth (default 512).
+   * @param int    $flags Bitmask of JSON decode options (default 0).
    * @return bool True if the string is valid JSON, false otherwise.
    */
-  function json_validate($string)
+  function json_validate(string $json, int $depth = 512, int $flags = 0): bool
   {
-    json_decode($string);
+    json_decode($json, false, $depth, $flags);
     return (json_last_error() === JSON_ERROR_NONE);
   }
 }
