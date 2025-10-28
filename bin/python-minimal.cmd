94f925fe2d5ffdc95dff85492e6a09a29b0e3aa2
dimaslanjaka <dimaslanjaka@gmail.com>
2025-10-28 11:50:31 +0700

    fix: replace 'py' with explicit path to Python executable in installation commands

--- bin/python-minimal.cmd
+++ bin/python-minimal.cmd
@@ -32,25 +32,27 @@ if not exist venv (
   %PY_CREATOR% -m venv venv
 )
 call venv\Scripts\activate.bat
-py -m pip install --upgrade pip
-py -m pip install requests PySocks beautifulsoup4 lxml httpx
+
+set "PY=%CWD%\venv\Scripts\python.exe"
+%PY% -m pip install --upgrade pip
+%PY% -m pip install requests PySocks beautifulsoup4 lxml httpx
 
 if exist packages/proxy-checker-python (
-    py -m pip install -e packages/proxy-checker-python
+    %PY% -m pip install -e packages/proxy-checker-python
 ) else (
-    py -m pip install git+https://github.com/dimaslanjaka/proxy-checker-python.git@master
+    %PY% -m pip install git+https://github.com/dimaslanjaka/proxy-checker-python.git@master
 )
 
 if exist packages/proxy-hunter-python (
-    py -m pip install -e packages/proxy-hunter-python
+    %PY% -m pip install -e packages/proxy-hunter-python
 ) else (
-    py -m pip install "git+https://github.com/dimaslanjaka/proxy-hunter-python.git@master#subdirectory=packages/proxy-hunter-python"
+    %PY% -m pip install "git+https://github.com/dimaslanjaka/proxy-hunter-python.git@master#subdirectory=packages/proxy-hunter-python"
 )
 
 if exist packages/rsa-utility (
-    py -m pip install -e packages/rsa-utility
+    %PY% -m pip install -e packages/rsa-utility
 ) else (
-    py -m pip install "git+https://github.com/dimaslanjaka/php-proxy-hunter.git@master#subdirectory=packages/rsa-utility"
+    %PY% -m pip install "git+https://github.com/dimaslanjaka/php-proxy-hunter.git@master#subdirectory=packages/rsa-utility"
 )
 
 endlocal
