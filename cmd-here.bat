9d636edd245c5ebebc62c6f407814cd0fe880aec
dimaslanjaka <dimaslanjaka@gmail.com>
2025-11-03 16:46:30 +0700

    refactor(cmd-here): update CUSTOM_PATH to include Nox directories for improved environment setup

--- cmd-here.bat
+++ cmd-here.bat
@@ -5,7 +5,7 @@ REM Remove trailing backslash if present
 if "%WORKSPACE_FOLDER:~-1%"=="\" set "WORKSPACE_FOLDER=%WORKSPACE_FOLDER:~0,-1%"
 
 REM Build the custom PATH
-set "CUSTOM_PATH=%LOCALAPPDATA%\nvm;C:\nvm4w\nodejs;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin;%PATH%;%WORKSPACE_FOLDER%\node_modules\.bin;%WORKSPACE_FOLDER%\bin;%WORKSPACE_FOLDER%\vendor\bin"
+set "CUSTOM_PATH=%LOCALAPPDATA%\nvm;C:\nvm4w\nodejs;C:\Program Files\Nox\bin;D:\Program Files\Nox\bin;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin;%PATH%;%WORKSPACE_FOLDER%\node_modules\.bin;%WORKSPACE_FOLDER%\bin;%WORKSPACE_FOLDER%\vendor\bin"
 
 REM Open a new cmd window with the custom PATH and current directory
 start "" cmd.exe /K "set PATH=%CUSTOM_PATH% && cd /d %CD%"
