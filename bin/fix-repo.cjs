55c0a7460016bdbcc953f024f2626570d7e5965a
dimaslanjaka <dimaslanjaka@gmail.com>
2026-01-16 03:59:24 +0700

    chore(bin): add Node-based fix-repo and remove legacy git-merge scripts
    - add `bin/fix-repo.cjs` implementing `fixRemotes()` with `git-command-helper.spawnAsync`
    - update `bin/fix-repo` to invoke `node "$REPO_PATH/bin/fix-repo.cjs"`
    - remove legacy wrapper/scripts: `bin/git-merge`, `bin/git-merge.cjs`, `bin/git-merge.cmd`
    
    Centralizes repository remote fixes in a Node script and removes outdated shell/Windows wrappers.

---
+++ bin/fix-repo.cjs
@@ -0,0 +1,47 @@
+const { path, noop } = require('sbg-utility');
+const gch = require('git-command-helper');
+require('dotenv').config({ path: path.join(__dirname, '/../'), override: true });
+
+const GH_TOKEN = process.env.ACCESS_TOKEN;
+
+async function fixRemotes() {
+  await gch
+    .spawnAsync(
+      'git',
+      ['remote', 'add', 'origin', `https://dimaslanjaka:${GH_TOKEN}@github.com/dimaslanjaka/php-proxy-hunter.git`],
+      { stdio: 'inherit' }
+    )
+    .then(noop)
+    .catch(noop);
+  await gch
+    .spawnAsync(
+      'git',
+      ['remote', 'set-url', 'origin', `https://dimaslanjaka:${GH_TOKEN}@github.com/dimaslanjaka/php-proxy-hunter.git`],
+      { stdio: 'inherit' }
+    )
+    .then(noop)
+    .catch(noop);
+  await gch
+    .spawnAsync(
+      'git',
+      ['remote', 'add', 'private', `https://dimaslanjaka:${GH_TOKEN}@github.com/dimaslanjaka/traffic-generator.git`],
+      { stdio: 'inherit' }
+    )
+    .then(noop)
+    .catch(noop);
+  await gch
+    .spawnAsync(
+      'git',
+      [
+        'remote',
+        'set-url',
+        'private',
+        `https://dimaslanjaka:${GH_TOKEN}@github.com/dimaslanjaka/traffic-generator.git`
+      ],
+      { stdio: 'inherit' }
+    )
+    .then(noop)
+    .catch(noop);
+}
+
+fixRemotes().catch(noop);
