ab24d9f7004c0023488cc67b1fd18de8480b6648
dimaslanjaka <dimaslanjaka@gmail.com>
2026-03-24 06:47:12 +0700

    fix: refactor Puppeteer configuration - skip download

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,9 +1,25 @@
-const { join } = require('path');
+const path = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
  */
-module.exports = {
+const config = {
   // Cache location for Puppeteer.
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer')
+  cacheDirectory: path.join(__dirname, '.cache', 'puppeteer'),
+  // Download Chrome (default `skipDownload: false`).
+  chrome: {
+    skipDownload: true
+  },
+  // Download Firefox (default `skipDownload: true`).
+  firefox: {
+    skipDownload: true
+  },
+  temporaryDirectory: path.join(__dirname, 'tmp/puppeteer')
 };
+
+const fs = require('fs');
+
+fs.mkdirSync(config.cacheDirectory, { recursive: true });
+fs.mkdirSync(config.temporaryDirectory, { recursive: true });
+
+module.exports = config;
